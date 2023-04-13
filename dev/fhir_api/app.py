972c8d7be963a9bd2dc27e32f122853d415c2ca5
nathanwall <me+github@nathanwall.co.uk>
2023-04-13 14:15:31 +0100

    mangum handler for AWS lambda

---
+++ dev/fhir_api/app.py
@@ -0,0 +1,32 @@
+'''
+APP for fastapi
+'''
+import os
+from fastapi import FastAPI
+from mangum import Mangum
+from fhir_api.routes import (
+    immunisation,
+    root,
+    health,
+    snomed,
+)
+
+from fhir_api.models.fhir_r4.common import Reference, Identifier
+
+Reference.update_forward_refs(identifier=Identifier)
+
+
+app = FastAPI(
+    title=os.getenv('FASTAPI_TITLE', 'Immunization Fhir API'),
+    description=os.getenv(
+        'FASTAPI_DESC', 'API'),
+    version=os.getenv('VERSION', 'DEVELOPMENT'))
+
+
+# ENDPOINT ROUTERS
+app.include_router(root.router)
+app.include_router(health.router)
+app.include_router(immunisation.router)
+app.include_router(snomed.router)
+
+handler = Mangum(app)
