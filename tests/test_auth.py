c1bf44e175757a05070c10080848a2a0b20d0dc0
ewan-IW <ewan.childs@infinityworks.com>
2023-10-02 18:05:06 +0100

    Adding more scenarios for 401 to be caught

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,7 +5,6 @@ from .example_loader import load_example
 
 
 @pytest.mark.auth
-@pytest.mark.debug
 @pytest.mark.nhsd_apim_authorization(
     {
         "access": "healthcare_worker",
@@ -24,9 +23,24 @@ def test_invalid_operation_returns_400(nhsd_apim_auth_headers):
 
 
 @pytest.mark.auth
-def test_invalid_access_token_returns_401():
-    endpoint_url = f"{config.BASE_URL}/{config.BASE_PATH}"
+@pytest.mark.debug
+def test_invalid_access_token():
+    expected_status_code = 401
     expected_body = load_example("OperationOutcome/401-invalid_access_token.json")
+
+    response = requests.get(
+        url=f"{config.BASE_URL}/{config.BASE_PATH}",
+        headers={
+            "Authorization": "Bearer invalid_token"
+        },
+    )
+    assert response.status_code == expected_status_code
+    assert response.json() == expected_body
+    
+@pytest.mark.auth
+def test_invalid_access_token_returns_403():
+    endpoint_url = f"{config.BASE_URL}/{config.BASE_PATH}"
+    expected_body = load_example("OperationOutcome/403-unauthorized.json")
     invalid_token = "invalid-access-token"
 
     headers = {
@@ -37,5 +51,5 @@ def test_invalid_access_token_returns_401():
 
     response = requests.get(endpoint_url, headers=headers)
 
-    assert response.status_code == 401
+    assert response.status_code == 403
     assert response.json() == expected_body
