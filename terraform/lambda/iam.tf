cfa759be990f36aa5a4ad7cbd4182d483502013f
ewan-IW <ewan.childs@infinityworks.com>
2023-10-02 17:45:10 +0100

    Fixing more role policy spacing errors

--- terraform/lambda/iam.tf
+++ terraform/lambda/iam.tf
@@ -1,19 +1,19 @@
 resource "aws_iam_role" "lambda_role" {
   name = "${var.short_prefix}-lambda-role"
   assume_role_policy =<<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
 EOF
 }
 
@@ -21,7 +21,6 @@ EOF
 resource "aws_iam_role_policy" "lambda_role_policy" {
   name = "${var.prefix}-policy"
   role = aws_iam_role.lambda_role.id
-
   policy = <<EOF
 {
   "Version": "2012-10-17",
