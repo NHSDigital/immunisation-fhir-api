1e599785317adc325d59171f0683ccfcead53c6c
ewan-IW <113019175+ewan-IW@users.noreply.github.com>
2023-10-03 11:04:15 +0100

    Update environment.py

--- tests/configuration/environment.py
+++ tests/configuration/environment.py
@@ -13,12 +13,6 @@ def get_env(variable_name: str) -> str:
 
 
 ENV = {
-    # Apigee
     "environment": get_env("APIGEE_ENVIRONMENT"),
     "base_path": get_env("SERVICE_BASE_PATH"),
-    # "proxy_name": get_env("FULLY_QUALIFIED_SERVICE_NAME"),
-    # "client_id": get_env("CLIENT_ID"),
-    # "client_secret": get_env("CLIENT_SECRET"),
-    # "redirect_url": get_env("REDIRECT_URL")
-    # "target_system": get_env("TARGET_SYSTEM"),
 }
