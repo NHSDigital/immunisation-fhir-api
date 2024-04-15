1b8431b1a25be0a9353edf044e297f53a058b209
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-04-15 16:44:21 +0100

    Added account instead of service

--- terraform/delta.tf
+++ terraform/delta.tf
@@ -116,7 +116,7 @@ resource "aws_iam_role" "dynamo_s3_access_role" {
    {
      "Effect": "Allow",
      "Principal": {
-       "Service": "arn:aws:iam::${local.account_id}:root"
+       "Account": "arn:aws:iam::${local.account_id}:root"
      },
      "Action": "sts:AssumeRole"
    }
