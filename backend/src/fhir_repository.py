d4393789c7e7230bdab8f0fb08a02aba7da8025c
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-07-24 19:26:31 +0100

    Added print statement

--- backend/src/fhir_repository.py
+++ backend/src/fhir_repository.py
@@ -94,7 +94,7 @@ class ImmunizationRepository:
 
     def get_immunization_by_id(self, imms_id: str, imms_vax_type_perms: str) -> Optional[dict]:
         response = self.table.get_item(Key={"PK": _make_immunization_pk(imms_id)})
-
+        print(f"response:{response}")
         if "Item" in response:
             if "DeletedAt" in response["Item"]:
                 return None
--- backend/src/get_imms_handler.py
+++ backend/src/get_imms_handler.py
@@ -10,11 +10,13 @@ from log_structure import function_info
 
 @function_info
 def get_imms_handler(event, context): 
+    print("1")
     return get_immunization_by_id(event, make_controller())
 
 
 def get_immunization_by_id(event, controller: FhirController):
     try:
+        print("2")
         return controller.get_immunization_by_id(event)
     except Exception as e:
         exp_error = create_operation_outcome(resource_id=str(uuid.uuid4()), severity=Severity.error,
@@ -27,6 +29,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("get_imms_handler")
     parser.add_argument("id", help="Id of Immunization.", type=str)
     args = parser.parse_args()
+    print(f"parser{parser},,,{args}")
 
     event = {
         "pathParameters": {
