1eebddffbd7f9c99cded08a8b0c4e78ab77b5516
nhsdevws <stephen.wates1@nhs.net>
2025-04-12 15:13:44 +0100

    clean

--- delta_backend/tests/test_delta.py
+++ delta_backend/tests/test_delta.py
@@ -1,17 +1,7 @@
 import unittest
-import os
-from unittest.mock import patch, MagicMock
-from botocore.exceptions import ClientError
-from sample_data.test_resource_data import get_test_data_resource
+from unittest.mock import patch
 
-# Set environment variables before importing the module
-os.environ["AWS_SQS_QUEUE_URL"] = "https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue"
-os.environ["DELTA_TABLE_NAME"] = "my_delta_table"
-os.environ["SOURCE"] = "my_source"
-os.environ["SPLUNK_FIREHOSE_NAME"] = "my_firehose"
-
-from src.delta import send_message, handler  # Import after setting environment variables
-import json
+from src.delta import handler  # Import after setting environment variables
 
 
 class DeltaTestCase(unittest.TestCase):
@@ -20,10 +10,7 @@ class DeltaTestCase(unittest.TestCase):
         # Common setup if needed
         self.context = {}
 
-    @patch("delta.firehose_logger")
-    @patch("boto3.client") 
-    @patch("delta.boto3.resource")
-    def test_handler(self, mock_boto_resource, mock_boto_client, mock_firehose_logger):
+    def test_handler(self):
         # Arrange
         event = { "text": "hello world"}
 
