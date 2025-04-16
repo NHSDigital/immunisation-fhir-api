c0d333e3353e65ad3a8cc1593963063594c816ac
nhsdevws <stephen.wates1@nhs.net>
2025-04-16 17:37:37 +0100

    test_conversionCount commented

--- delta_backend/tests/test_convert_to_flat_json.py
+++ delta_backend/tests/test_convert_to_flat_json.py
@@ -99,94 +99,94 @@ class TestConvertToFlatJson(unittest.TestCase):
             self.assertIn(key, filtered_items[0], f"{key} is missing")
             self.assertEqual(filtered_items[0][key], expected_value, f"{key} mismatch")
 
-#     def test_fhir_converter_json_direct_data(self):
-#         """it should convert fhir json data to flat json"""
-#         imms.clear()
-#         json_data = json.dumps(ValuesForTests.json_data)
+    def test_fhir_converter_json_direct_data(self):
+        """it should convert fhir json data to flat json"""
+        imms.clear()
+        json_data = json.dumps(ValuesForTests.json_data)
 
-#         start = time.time()
+        start = time.time()
 
-#         FHIRConverter = Converter(json_data)
-#         FlatFile = FHIRConverter.runConversion(ValuesForTests.json_data, False, True)
+        FHIRConverter = Converter(json_data)
+        FlatFile = FHIRConverter.runConversion(ValuesForTests.json_data, False, True)
 
-#         flatJSON = json.dumps(FlatFile)
-#         expected_imms_value = deepcopy(ValuesForTests.expected_imms)  # UPDATE is currently the default action-flag
-#         expected_imms = json.dumps(expected_imms_value)
-#         self.assertEqual(flatJSON, expected_imms)
+        flatJSON = json.dumps(FlatFile)
+        expected_imms_value = deepcopy(ValuesForTests.expected_imms)  # UPDATE is currently the default action-flag
+        expected_imms = json.dumps(expected_imms_value)
+        self.assertEqual(flatJSON, expected_imms)
 
-#         errorRecords = FHIRConverter.getErrorRecords()
-#         # print(flatJSON)
+        errorRecords = FHIRConverter.getErrorRecords()
+        # print(flatJSON)
 
-#         if len(errorRecords) > 0:
-#             print("Converted With Errors")
-#         else:
-#             print("Converted Successfully")
+        if len(errorRecords) > 0:
+            print("Converted With Errors")
+        else:
+            print("Converted Successfully")
 
-#         end = time.time()
-#         print(end - start)
+        end = time.time()
+        print(end - start)
 
-#     def test_fhir_converter_json_error_scenario(self):
-#         """it should convert fhir json data to flat json - error scenarios"""
-#         error_test_cases = [ErrorValuesForTests.missing_json, ErrorValuesForTests.json_dob_error]
+    def test_fhir_converter_json_error_scenario(self):
+        """it should convert fhir json data to flat json - error scenarios"""
+        error_test_cases = [ErrorValuesForTests.missing_json, ErrorValuesForTests.json_dob_error]
 
-#         for test_case in error_test_cases:
-#             imms.clear()
-#             json_data = json.dumps(test_case)
+        for test_case in error_test_cases:
+            imms.clear()
+            json_data = json.dumps(test_case)
 
-#             start = time.time()
+            start = time.time()
 
-#             FHIRConverter = Converter(json_data)
-#             FlatFile = FHIRConverter.runConversion(ValuesForTests.json_data, False, True)
+            FHIRConverter = Converter(json_data)
+            FlatFile = FHIRConverter.runConversion(ValuesForTests.json_data, False, True)
 
-#             flatJSON = json.dumps(FlatFile)
+            flatJSON = json.dumps(FlatFile)
 
-#             # if len(flatJSON) > 0:
-#             #     print(flatJSON)
-#             # Fix error handling
-#             # expected_imms = ErrorValuesForTests.get_expected_imms_error_output
-#             # self.assertEqual(flatJSON, expected_imms)
+            # if len(flatJSON) > 0:
+            #     print(flatJSON)
+            # Fix error handling
+            # expected_imms = ErrorValuesForTests.get_expected_imms_error_output
+            # self.assertEqual(flatJSON, expected_imms)
 
-#             errorRecords = FHIRConverter.getErrorRecords()
+            errorRecords = FHIRConverter.getErrorRecords()
 
-#             if len(errorRecords) > 0:
-#                 print("Converted With Errors")
-#                 print(f"Error records -error scenario {errorRecords}")
-#             else:
-#                 print("Converted Successfully")
+            if len(errorRecords) > 0:
+                print("Converted With Errors")
+                print(f"Error records -error scenario {errorRecords}")
+            else:
+                print("Converted Successfully")
 
-#             end = time.time()
-#             print(end - start)
+            end = time.time()
+            print(end - start)
 
-#     def test_handler_imms_convert_to_flat_json(self):
-#         """Test that the Imms field contains the correct flat JSON data for CREATE, UPDATE, and DELETE operations."""
-#         expected_action_flags = [
-#             {"Operation": "CREATE", "EXPECTED_ACTION_FLAG": "NEW"},
-#             {"Operation": "UPDATE", "EXPECTED_ACTION_FLAG": "UPDATE"},
-#             {"Operation": "DELETE", "EXPECTED_ACTION_FLAG": "DELETE"},
-#         ]
+    def test_handler_imms_convert_to_flat_json(self):
+        """Test that the Imms field contains the correct flat JSON data for CREATE, UPDATE, and DELETE operations."""
+        expected_action_flags = [
+            {"Operation": "CREATE", "EXPECTED_ACTION_FLAG": "NEW"},
+            {"Operation": "UPDATE", "EXPECTED_ACTION_FLAG": "UPDATE"},
+            {"Operation": "DELETE", "EXPECTED_ACTION_FLAG": "DELETE"},
+        ]
 
-#         for test_case in expected_action_flags:
-#             with self.subTest(test_case["Operation"]):
-#                 imms.clear()
+        for test_case in expected_action_flags:
+            with self.subTest(test_case["Operation"]):
+                imms.clear()
 
-#                 event = self.get_event(operation=test_case["Operation"])
+                event = self.get_event(operation=test_case["Operation"])
 
-#                 response = handler(event, None)
+                response = handler(event, None)
 
-#                 # Retrieve items from DynamoDB
-#                 result = self.table.scan()
-#                 items = result.get("Items", [])
+                # Retrieve items from DynamoDB
+                result = self.table.scan()
+                items = result.get("Items", [])
 
-#                 expected_values = ValuesForTests.expected_static_values
-#                 expected_imms = ValuesForTests.get_expected_imms(test_case["EXPECTED_ACTION_FLAG"])
+                expected_values = ValuesForTests.expected_static_values
+                expected_imms = ValuesForTests.get_expected_imms(test_case["EXPECTED_ACTION_FLAG"])
 
-#                 self.assert_dynamodb_record(
-#                     test_case["EXPECTED_ACTION_FLAG"], items, expected_values, expected_imms, response
-#                 )
+                self.assert_dynamodb_record(
+                    test_case["EXPECTED_ACTION_FLAG"], items, expected_values, expected_imms, response
+                )
 
-#                 result = self.table.scan()
-#                 items = result.get("Items", [])
-#                 self.clear_table()
+                result = self.table.scan()
+                items = result.get("Items", [])
+                self.clear_table()
 
 #     def test_conversionCount(self):
 #         parser = SchemaParser()
