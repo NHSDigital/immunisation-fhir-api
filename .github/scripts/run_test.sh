2f8ee3175a57964ef1c762b10743d884f138bce8
nhsdevws <stephen.wates1@nhs.net>
2025-05-31 15:21:18 +0100

    check cache

--- .github/scripts/run_test.sh
+++ .github/scripts/run_test.sh
@@ -6,9 +6,16 @@ DESCRIPTION="$2"
 COVERAGE_XML="sonarcloud-coverage-$3.xml"
 
 echo "Using Python $PYTHON_VERSION"
-poetry env use "$PYTHON_VERSION"
 poetry config virtualenvs.in-project true
-poetry install
+
+# Only create/use the env and install if .venv does not exist
+if [ ! -d ".venv" ]; then
+  echo "Creating virtual environment (.venv) with Poetry"
+  poetry env use "$PYTHON_VERSION"
+  poetry install
+else
+  echo "Using cached virtual environment (.venv)"
+fi
 
 if poetry run coverage run -m unittest discover; then
   echo "$DESCRIPTION tests passed"
@@ -17,4 +24,3 @@ else
 fi
 
 poetry run coverage xml -o "../$COVERAGE_XML"
-# poetry env list --full-path | awk '{print $1}' | xargs -n 1 poetry env remove || true
