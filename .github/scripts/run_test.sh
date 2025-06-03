c7180c753cb7ece5a053ba61638f2759abb69aa2
nhsdevws <stephen.wates1@nhs.net>
2025-06-03 11:43:11 +0100

    Script

--- .github/scripts/run_test.sh
+++ .github/scripts/run_test.sh
@@ -5,15 +5,7 @@ PYTHON_VERSION="$1"
 DESCRIPTION="$2"
 COVERAGE_XML="sonarcloud-coverage-$3.xml"
 
-
-          poetry env use 3.10
-          poetry install
-          poetry run coverage run -m unittest discover || echo "mesh_processor tests failed" >> ../failed_tests.txt
-          poetry run coverage xml -o ../sonarcloud-mesh_processor-coverage.xml
-
-
-  poetry env use "$PYTHON_VERSION"
-  poetry install
-  poetry run coverage run -m unittest discover || echo "$DESCRIPTION tests failed" >> ../failed_tests.txt
-
+poetry env use "$PYTHON_VERSION"
+poetry install
+poetry run coverage run -m unittest discover || echo "$DESCRIPTION tests failed" >> ../failed_tests.txt
 poetry run coverage xml -o "../$COVERAGE_XML"
