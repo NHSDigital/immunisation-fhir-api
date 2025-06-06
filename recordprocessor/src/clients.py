5603ea4f38da363ed390a78c8a3b60c7b6498509
nhsdevws <stephen.wates1@nhs.net>
2025-06-06 14:53:07 +0100

    WIP

--- recordprocessor/src/clients.py
+++ recordprocessor/src/clients.py
@@ -29,3 +29,5 @@ REDIS_HOST = os.getenv("REDIS_HOST", "immunisation-redis-cluster.0y9mwl.0001.euw
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 
 redis_cacher = RedisCacher(REDIS_HOST, REDIS_PORT, logger)
+
+disease_vaccine_mapping = DiseaseMapping(redis_cacher)
