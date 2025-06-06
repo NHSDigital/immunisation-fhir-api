b39d27cbc57d242b2b10326343f7303b005c8700
nhsdevws <stephen.wates1@nhs.net>
2025-06-06 19:57:07 +0100

    s3 trigger

--- terraform/redis_sync_lambda.tf
+++ terraform/redis_sync_lambda.tf
@@ -1,35 +1,8 @@
+# Define the directory containing source code and calculate its SHA-256 hash for triggering redeployments
 locals {
-  redis_sync_dir = abspath("${path.root}/../redis_sync")
-  redis_sync_files = fileset(local.redis_sync_dir, "**")
+  redis_sync_dir     = abspath("${path.root}/../redis_sync_lambda")
+  redis_sync_files   = fileset(local.redis_sync_dir, "**")
   redis_sync_dir_sha = sha1(join("", [for f in local.redis_sync_files : filesha1("${local.redis_sync_dir}/${f}")]))
-  function_name = "redis_sync"
-  dlq_name = "redis_sync-dlq"
-  sns_name = "redis_sync-sns"
-}
-
-resource "aws_iam_role" "redis_sync_lambda_role" {
-  name               = "${local.short_prefix}-${local.function_name}-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "redis_sync_lambda_role_policy" {
-  name   = "${local.prefix}-${local.function_name}-policy"
-  role   = aws_iam_role.redis_sync_lambda_role.id
-  policy = data.aws_iam_policy_document.redis_sync_policy_document.json
 }
 
 data "archive_file" "redis_sync_lambda_zip" {
@@ -39,73 +12,324 @@ data "archive_file" "redis_sync_lambda_zip" {
 }
 
 resource "aws_lambda_function" "redis_sync_lambda" {
-  function_name = "${local.short_prefix}-${local.function_name}"
-  role          = aws_iam_role.redis_sync_lambda_role.arn
-  handler       = "redis_sync.sync_handler" # Update as appropriate
+  function_name = "${local.short_prefix}-redis-sync-lambda"
+  role          = aws_iam_role.redis_sync_lambda_exec_role.arn
+  handler       = "redis_sync_handler.sync_handler" # Update as appropriate
   runtime       = "python3.11"
   filename      = data.archive_file.redis_sync_lambda_zip.output_path
   source_code_hash = data.archive_file.redis_sync_lambda_zip.output_base64sha256
   architectures = ["x86_64"]
+  timeout       = 360
+
+  vpc_config {
+    subnet_ids         = data.aws_subnets.default.ids
+    security_group_ids = [data.aws_security_group.existing_securitygroup.id]
+  }
 
   environment {
     variables = {
-      DELTA_TABLE_NAME     = aws_dynamodb_table.delta-dynamodb-table.name
-      AWS_SQS_QUEUE_URL    = aws_sqs_queue.dlq.id
-      SOURCE               = "IEDS"
-      SPLUNK_FIREHOSE_NAME = module.splunk.firehose_stream_name
+      SOURCE_BUCKET_NAME         = aws_s3_bucket.batch_data_source_bucket.bucket
+      ACK_BUCKET_NAME            = aws_s3_bucket.batch_data_destination_bucket.bucket
+      CONFIG_BUCKET_NAME         = local.config_bucket_name
+      REDIS_HOST                 = data.aws_elasticache_cluster.existing_redis.cache_nodes[0].address
+      REDIS_PORT                 = data.aws_elasticache_cluster.existing_redis.cache_nodes[0].port
+      SPLUNK_FIREHOSE_NAME       = module.splunk.firehose_stream_name
+      REDIS_SYNC_PROC_LAMBDA_NAME = "imms-${local.env}-redis_sync_lambda"
     }
   }
-
+  kms_key_arn                    = data.aws_kms_key.existing_lambda_encryption_key.arn
+  reserved_concurrent_executions = local.is_temp ? -1 : 20
   depends_on = [
-    aws_cloudwatch_log_group.redis_sync_lambda
+    aws_cloudwatch_log_group.redis_sync_lambda_log_group,
+    aws_iam_policy.redis_sync_lambda_exec_policy
   ]
 }
 
-resource "aws_cloudwatch_log_group" "redis_sync_lambda" {
-  name              = "/aws/lambda/${local.short_prefix}-${local.function_name}"
-  retention_in_days = 30
+# Permission for S3 to invoke Lambda function
+resource "aws_lambda_permission" "redis_sync_s3_invoke_permission" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.redis_sync_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::imms-${local.env}-supplier-config"
 }
 
-resource "aws_lambda_event_source_mapping" "redis_sync_trigger" {
-  event_source_arn  = aws_dynamodb_table.events-dynamodb-table.stream_arn
-  function_name     = aws_lambda_function.redis_sync_lambda.function_name
-  starting_position = "TRIM_HORIZON"
-  destination_config {
-    on_failure {
-      destination_arn = aws_sns_topic.redis_sync_sns.arn
-    }
+# S3 Bucket notification to trigger Lambda function
+resource "aws_s3_bucket_notification" "redis_sync_lambda_notification" {
+  bucket = "imms-${local.env}-supplier-config"
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.redis_sync_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    # filter_prefix      = ""
+    # filter_suffix      = ""
   }
-  maximum_retry_attempts = 0
+
+  depends_on = [aws_lambda_permission.redis_sync_s3_invoke_permission]
 }
 
-resource "aws_sqs_queue" "dlq" {
-  name = "${local.short_prefix}-${local.dlq_name}"
+# IAM Role for Lambda
+resource "aws_iam_role" "redis_sync_lambda_exec_role" {
+  name = "${local.short_prefix}-redis-sync-lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Sid    = "",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_sns_topic" "redis_sync_sns" {
-  name = "${local.short_prefix}-${local.sns_name}"
+# Policy for Lambda execution role
+resource "aws_iam_policy" "redis_sync_lambda_exec_policy" {
+  name = "${local.short_prefix}-redis-sync-lambda-exec-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${var.aws_region}:${local.immunisation_account_id}:log-group:/aws/lambda/${local.short_prefix}-redis_sync_lambda:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:CopyObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.batch_data_source_bucket.arn,
+          "${aws_s3_bucket.batch_data_source_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.batch_data_destination_bucket.arn,
+          "${aws_s3_bucket.batch_data_destination_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          local.config_bucket_arn,
+          "${local.config_bucket_arn}/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "firehose:PutRecord",
+          "firehose:PutRecordBatch"
+        ],
+        "Resource" : "arn:aws:firehose:*:*:deliverystream/${module.splunk.firehose_stream_name}"
+      },
+      {
+        Effect = "Allow"
+        Action = "lambda:InvokeFunction"
+        Resource = [
+          "arn:aws:lambda:${var.aws_region}:${local.immunisation_account_id}:function:imms-${local.env}-redis_sync_lambda",
+        ]
+      }
+    ]
+  })
 }
 
-data "aws_iam_policy_document" "redis_sync_policy_document" {
-  source_policy_documents = [
-    templatefile("${local.policy_path}/dynamodb.json", {
-      "dynamodb_table_name" : aws_dynamodb_table.delta-dynamodb-table.name
-    }),
-    templatefile("${local.policy_path}/dynamodb_stream.json", {
-      "dynamodb_table_name" : aws_dynamodb_table.events-dynamodb-table.name
-    }),
-    templatefile("${local.policy_path}/aws_sqs_queue.json", {
-      "aws_sqs_queue_name" : aws_sqs_queue.dlq.name
-    }),
-    templatefile("${local.policy_path}/dynamo_key_access.json", {
-      "dynamo_encryption_key" : data.aws_kms_key.existing_dynamo_encryption_key.arn
-    }),
-    templatefile("${local.policy_path}/aws_sns_topic.json", {
-      "aws_sns_topic_name" : aws_sns_topic.redis_sync_sns.name
-    }),
-    templatefile("${local.policy_path}/log_kinesis.json", {
-      "kinesis_stream_name" : module.splunk.firehose_stream_name
-    }),
-    templatefile("${local.policy_path}/log.json", {}),
-  ]
+resource "aws_iam_policy" "redis_sync_lambda_kms_access_policy" {
+  name        = "${local.short_prefix}-redis-sync-lambda-kms-policy"
+  description = "Allow Lambda to decrypt environment variables"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt"
+        ]
+        Resource = data.aws_kms_key.existing_lambda_encryption_key.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = [
+          data.aws_kms_key.existing_s3_encryption_key.arn,
+          data.aws_kms_key.existing_dynamo_encryption_key.arn
+        ]
+      }
+    ]
+  })
+}
+
+
+# Attach the execution policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "redis_sync_lambda_exec_policy_attachment" {
+  role       = aws_iam_role.redis_sync_lambda_exec_role.name
+  policy_arn = aws_iam_policy.redis_sync_lambda_exec_policy.arn
+}
+
+# Attach the kms policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "redis_sync_lambda_kms_policy_attachment" {
+  role       = aws_iam_role.redis_sync_lambda_exec_role.name
+  policy_arn = aws_iam_policy.redis_sync_lambda_kms_access_policy.arn
+}
+
+
+# Permission for S3 to invoke Lambda function
+resource "aws_lambda_permission" "s3_invoke_permission" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_processor_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.batch_data_source_bucket.arn
+}
+
+# S3 Bucket notification to trigger Lambda function
+resource "aws_s3_bucket_notification" "datasources_lambda_notification" {
+  bucket = aws_s3_bucket.batch_data_source_bucket.bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.file_processor_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    #filter_prefix      =""
+  }
+}
+
+# S3 Bucket notification to trigger Lambda function for config bucket
+resource "aws_s3_bucket_notification" "config_lambda_notification" {
+  # For now, only create a trigger in internal-dev and prod as those are the envs with a config bucket
+  count = local.create_config_bucket ? 1 : 0
+
+  bucket = aws_s3_bucket.batch_config_bucket[0].bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.file_processor_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+# Permission for the new S3 bucket to invoke the Lambda function
+resource "aws_lambda_permission" "new_s3_invoke_permission" {
+  count = local.create_config_bucket ? 1 : 0
+
+  statement_id  = "AllowExecutionFromNewS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_processor_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = local.config_bucket_arn
+}
+
+# IAM Role for ElastiCache.
+resource "aws_iam_role" "elasticache_exec_role" {
+  name = "${local.short_prefix}-elasticache-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Sid    = "",
+      Principal = {
+        Service = "elasticache.amazonaws.com" # ElastiCache service principal
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "elasticache_permissions" {
+  name = "${local.short_prefix}-elasticache-permissions"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticache:DescribeCacheClusters",
+          "elasticache:ListTagsForResource",
+          "elasticache:AddTagsToResource",
+          "elasticache:RemoveTagsFromResource"
+        ]
+        Resource = "arn:aws:elasticache:${var.aws_region}:${local.immunisation_account_id}:cluster/immunisation-redis-cluster"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticache:CreateCacheCluster",
+          "elasticache:DeleteCacheCluster",
+          "elasticache:ModifyCacheCluster"
+        ]
+        Resource = "arn:aws:elasticache:${var.aws_region}:${local.immunisation_account_id}:cluster/immunisation-redis-cluster"
+        Condition = {
+          "StringEquals" : {
+            "aws:RequestedRegion" : var.aws_region
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticache:DescribeCacheSubnetGroups"
+        ]
+        Resource = "arn:aws:elasticache:${var.aws_region}:${local.immunisation_account_id}:subnet-group/immunisation-redis-subnet-group"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticache:CreateCacheSubnetGroup",
+          "elasticache:DeleteCacheSubnetGroup",
+          "elasticache:ModifyCacheSubnetGroup"
+        ]
+        Resource = "arn:aws:elasticache:${var.aws_region}:${local.immunisation_account_id}:subnet-group/immunisation-redis-subnet-group"
+        Condition = {
+          "StringEquals" : {
+            "aws:RequestedRegion" : var.aws_region
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach the policy to the ElastiCache role
+resource "aws_iam_role_policy_attachment" "elasticache_policy_attachment" {
+  role       = aws_iam_role.elasticache_exec_role.name
+  policy_arn = aws_iam_policy.elasticache_permissions.arn
+}
+
+resource "aws_cloudwatch_log_group" "redis_sync_lambda_log_group" {
+  name              = "/aws/lambda/${local.short_prefix}-redis_sync_lambda"
+  retention_in_days = 30
 }
