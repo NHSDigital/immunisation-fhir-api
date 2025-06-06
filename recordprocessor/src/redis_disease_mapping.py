3043876a5c71347c4ad344c1091e18febdb7ec4b
nhsdevws <stephen.wates1@nhs.net>
2025-06-06 08:37:28 +0100

    tidy

--- recordprocessor/src/redis_disease_mapping.py
+++ recordprocessor/src/redis_disease_mapping.py
@@ -1,14 +1,39 @@
 "Upload the content from a config file in S3 to ElastiCache (Redis)"
-from redis_cacher import RedisCacher
 
+import json
+import redis
 from constants import DISEASE_MAPPING_FILE_KEY
+
+
+class RedisCacher():
+    """ RedisCacher abstraction class to decouple application code
+    from direct use of Redis client."""
+
+    def __init__(self, redis_host, redis_port, logger):
+        try:
+            # Attempt to connect to Redis
+            self.redis_client = redis.StrictRedis(redis_host, redis_port, decode_responses=True)
+            # Check the connection with a PING command
+            if self.redis_client.ping():
+                logger.info("Successfully connected to Redis.")
+            else:
+                logger.error("Failed to connect to Redis.")
+        except Exception as e:
+            logger.exception(f"Connection to Redis failed: {e}")
+
+    def get(self, key: str) -> dict:
+        """Gets the value from Redis cache for the given key."""
+        value = self.redis_client.get(key)
+        if value is not None:
+            return json.loads(value)
+        return {}
 
 
 class DiseaseMapping:
     """Class to handle disease mapping operations."""
     # redis_cache instance is found in clients.py
     def __init__(self, redis_cache: RedisCacher):
-        mapping = redis_cache.get_cache(DISEASE_MAPPING_FILE_KEY)
+        mapping = redis_cache.get(DISEASE_MAPPING_FILE_KEY)
         self.vaccines = mapping["vaccine"]
         self.diseases = mapping["disease"]
         self.load_vaccines_into_diseases()
@@ -43,14 +68,14 @@ class DiseaseMapping:
                 if vaccine not in self.diseases[disease]["vaccines"]:
                     self.diseases[disease]["vaccines"].append(vaccine)
 
-    def get_diseases(self, vaccine: str) -> list:
+    def get_diseases_from_vaccine(self, vaccine: str) -> list:
         """Returns a list of diseases for the given vaccine."""
         vaccine = self.vaccines.get(vaccine, {})
         if not vaccine:
             return []
         return vaccine.get("diseases", [])
 
-    def get_vaccines(self, disease: str) -> list:
+    def get_vaccines_from_disease(self, disease: str) -> list:
         """Returns a list of vaccines for the given disease."""
         disease = self.disease_map.get(disease, {})
         if not disease:
