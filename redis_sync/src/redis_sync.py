e146c2d8787480cb8590e34b844add62da4486a1
nhsdevws <stephen.wates1@nhs.net>
2025-06-11 09:36:10 +0100

    test logging

--- redis_sync/src/redis_sync.py
+++ redis_sync/src/redis_sync.py
@@ -1,4 +1,10 @@
-# handler to do redis sync from s3
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel("INFO")
+    
 
 def sync_handler(event, context):
-    print("Marker23. New code - no publish @ 0906")
+    print("Marker23. New code - no publish @ 0935")
+    logger.info("Marker23. New code - no publish @ 0936")
