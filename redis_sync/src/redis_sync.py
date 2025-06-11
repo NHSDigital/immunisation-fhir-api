a76e136e8116a3d0863369a0c26a880c427e63d6
nhsdevws <stephen.wates1@nhs.net>
2025-06-11 17:22:43 +0100

    pretty json

--- redis_sync/src/redis_sync.py
+++ redis_sync/src/redis_sync.py
@@ -1,3 +1,4 @@
+import json
 from clients import logger, redis_cacher
 from redis_disease_mapping import DiseaseMapping
 
@@ -9,7 +10,7 @@ def sync_handler(event, context):
     logger.info("Marker3. New code - no publish @ 0936")
 
     # handler is triggered by S3 event
-    logger.info("Event: %s", event)
+    logger.info("Event: %s", json.dumps(event, indent=2))
 
     # save the disease mapping to Redis
     logger.info("Saving disease mapping to Redis (TEST).")
