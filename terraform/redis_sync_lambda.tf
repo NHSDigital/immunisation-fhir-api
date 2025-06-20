4f0950394315c0887c90476d581dfd3e7d3c2d70
Matt Jarvis <matt.jarvis2@nhs.net>
2025-06-20 14:48:30 +0100

    VED-303: Use the existing build mechanism for the Redis sync lambda.

--- terraform/redis_sync_lambda.tf
+++ terraform/redis_sync_lambda.tf
@@ -1,115 +1,79 @@
-# Define the directory containing source code and calculate its SHA-256 hash for triggering redeployments
+# Define the directory containing the Docker image and calculate its SHA-256 hash for triggering redeployments
 locals {
-  redis_project_name = "redis_sync"
-  redis_sync_lambda_name = "${local.short_prefix}-redis-sync-lambda"
-  redis_sync_dir     = abspath("${path.root}/../${local.redis_project_name}")
-  build_dir        = "${local.redis_sync_dir}/build"
-  zip_file_name    = "${local.redis_project_name}.zip"
-  redis_sync_files   = fileset(local.redis_sync_dir, "**")
-  redis_sync_dir_sha = sha1(join("", [for f in local.redis_sync_files : filesha1("${local.redis_sync_dir}/${f}")]))
-  build_script = "${path.module}/package_lambda.sh"
-  redis_cloudwatch_group_name = "/aws/lambda/${local.redis_sync_lambda_name}"
+  redis_sync_lambda_dir     = abspath("${path.root}/../redis_sync")
+  redis_sync_lambda_files   = fileset(local.redis_sync_lambda_dir, "**")
+  redis_sync_lambda_dir_sha = sha1(join("", [for f in local.redis_sync_lambda_files : filesha1("${local.redis_sync_lambda_dir}/${f}")]))
 }
 
-output "redis_sync_dir" {
-  value = "redis_sync_dir: ${local.redis_sync_dir}"
-}
-
-output "redis_sync_files" {
-  value = "redis_sync_files: ${join(", ", local.redis_sync_files)}"
-}
-
-resource "null_resource" "make_build_dir" {
-  provisioner "local-exec" {
-    command = "mkdir -p ${local.build_dir} && echo \"Created build directory: ${local.build_dir}\""
+resource "aws_ecr_repository" "redis_sync_lambda_repository" {
+  image_scanning_configuration {
+    scan_on_push = true
   }
-  depends_on = [local.redis_sync_dir]
+  name         = "${local.short_prefix}-redis-sync-repo"
+  force_delete = local.is_temp
 }
 
-resource "null_resource" "chmod_package_lambda" {
-  provisioner "local-exec" {
-    command = "echo \"======SAW2 =====\" && ls -ltr  ${local.redis_sync_dir} && chmod +x ${local.build_script} && echo \"chmod done\""
-  }
-}
+# Module for building and pushing Docker image to ECR
+module "redis_sync_docker_image" {
+  source  = "terraform-aws-modules/lambda/aws//modules/docker-build"
+  version = "7.20.2"
 
-resource "null_resource" "package_lambda" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      chmod +x ${local.build_script} && \
-      ${local.build_script} ${local.redis_project_name} ${local.redis_sync_dir} ${local.build_dir} ${local.zip_file_name}
-    EOT
-  }
-  depends_on = [null_resource.chmod_package_lambda, null_resource.make_build_dir]
+  create_ecr_repo = false
+  ecr_repo        = aws_ecr_repository.redis_sync_lambda_repository.name
+  ecr_repo_lifecycle_policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep only the last 2 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 2
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+
+  platform      = "linux/amd64"
+  use_image_tag = false
+  source_path   = local.redis_sync_lambda_dir
   triggers = {
-    zip_exists_hash = fileexists("${local.build_dir}/${local.zip_file_name}") ? filesha1("${local.build_dir}/${local.zip_file_name}") : timestamp()
-
-    src_hash        = sha1(join("", fileset(local.redis_sync_dir, "**")))
-    pyproject_hash  = filesha1("${local.redis_sync_dir}/pyproject.toml")
-    poetry_lock_hash = filesha1("${local.redis_sync_dir}/poetry.lock")
-    build_script_hash = filesha1(local.build_script)
+    dir_sha = local.redis_sync_lambda_dir_sha
   }
 }
 
-data "archive_file" "redis_sync_lambda_zip" {
-  type        = "zip"
-  source_dir  = "${local.build_dir}"
-  output_path = "${local.redis_sync_dir}/${local.zip_file_name}"
+# Define the lambdaECRImageRetreival policy
+resource "aws_ecr_repository_policy" "redis_sync_lambda_ECRImageRetreival_policy" {
+  repository = aws_ecr_repository.redis_sync_lambda_repository.name
 
-  depends_on = [null_resource.package_lambda]
-}
-
-resource "aws_lambda_function" "redis_sync_lambda" {
-  function_name = local.redis_sync_lambda_name
-  role          = aws_iam_role.redis_sync_lambda_exec_role.arn
-  handler       = "redis_sync.sync_handler"
-  runtime       = "python3.11"
-  filename         = data.archive_file.redis_sync_lambda_zip.output_path
-  source_code_hash = data.archive_file.redis_sync_lambda_zip.output_base64sha256
-  architectures = ["x86_64"]
-  timeout       = 360
-
-  vpc_config {
-    subnet_ids         = data.aws_subnets.default.ids
-    security_group_ids = [data.aws_security_group.existing_securitygroup.id]
-  }
-
-  environment {
-    variables = {
-      CONFIG_BUCKET_NAME         = local.config_bucket_name
-      REDIS_HOST                 = data.aws_elasticache_cluster.existing_redis.cache_nodes[0].address
-      REDIS_PORT                 = data.aws_elasticache_cluster.existing_redis.cache_nodes[0].port
-      REDIS_SYNC_PROC_LAMBDA_NAME = "imms-${local.env}-redis_sync_lambda"
-    }
-  }
-  kms_key_arn                    = data.aws_kms_key.existing_lambda_encryption_key.arn
-  reserved_concurrent_executions = local.is_temp ? -1 : 20
-  depends_on = [
-    aws_cloudwatch_log_group.redis_sync_lambda_log_group,
-    aws_iam_policy.redis_sync_lambda_exec_policy
-  ]
-  publish = true
-}
-
-# Permission for S3 to invoke Lambda function
-resource "aws_lambda_permission" "redis_sync_s3_invoke_permission" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.redis_sync_lambda.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::imms-${local.config_bucket_env}-supplier-config"
-}
-
-# S3 Bucket notification to trigger Lambda function
-resource "aws_s3_bucket_notification" "redis_sync_lambda_notification" {
-  bucket = "imms-${local.config_bucket_env
-  }-supplier-config"
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.redis_sync_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-
-  depends_on = [aws_lambda_permission.redis_sync_s3_invoke_permission]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "LambdaECRImageRetrievalPolicy",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Action" : [
+          "ecr:BatchGetImage",
+          "ecr:DeleteRepositoryPolicy",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:SetRepositoryPolicy"
+        ],
+        "Condition" : {
+          "StringLike" : {
+            # "aws:sourceArn" : "arn:aws:lambda:eu-west-2:${local.immunisation_account_id}:function:${local.short_prefix}-redis_sync_lambda"
+            "aws:sourceArn" : aws_lambda_function.redis_sync_lambda.arn
+          }
+        }
+      }
+    ]
+  })
 }
 
 # IAM Role for Lambda
@@ -141,7 +105,33 @@ resource "aws_iam_policy" "redis_sync_lambda_exec_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
-        Resource = "arn:aws:logs:${var.aws_region}:${local.immunisation_account_id}:log-group:${local.redis_cloudwatch_group_name}:*"
+        Resource = "arn:aws:logs:${var.aws_region}:${local.immunisation_account_id}:log-group:/aws/lambda/${local.short_prefix}-redis_sync_lambda:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:CopyObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.batch_data_source_bucket.arn,
+          "${aws_s3_bucket.batch_data_source_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.batch_data_destination_bucket.arn,
+          "${aws_s3_bucket.batch_data_destination_bucket.arn}/*"
+        ]
       },
       {
         Effect = "Allow",
@@ -163,6 +153,14 @@ resource "aws_iam_policy" "redis_sync_lambda_exec_policy" {
           local.config_bucket_arn,
           "${local.config_bucket_arn}/*"
         ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "firehose:PutRecord",
+          "firehose:PutRecordBatch"
+        ],
+        "Resource" : "arn:aws:firehose:*:*:deliverystream/${module.splunk.firehose_stream_name}"
       },
       {
         Effect = "Allow"
@@ -198,65 +196,27 @@ resource "aws_iam_policy" "redis_sync_lambda_kms_access_policy" {
         ]
         Resource = [
           data.aws_kms_key.existing_s3_encryption_key.arn,
-          data.aws_kms_key.existing_dynamo_encryption_key.arn
         ]
       }
     ]
   })
 }
-
-
-# Attach the execution policy to the Lambda role
-resource "aws_iam_role_policy_attachment" "redis_sync_lambda_exec_policy_attachment" {
-  role       = aws_iam_role.redis_sync_lambda_exec_role.name
-  policy_arn = aws_iam_policy.redis_sync_lambda_exec_policy.arn
-}
-
-# Attach the kms policy to the Lambda role
-resource "aws_iam_role_policy_attachment" "redis_sync_lambda_kms_policy_attachment" {
-  role       = aws_iam_role.redis_sync_lambda_exec_role.name
-  policy_arn = aws_iam_policy.redis_sync_lambda_kms_access_policy.arn
-}
-
-# S3 Bucket notification to trigger Lambda function for config bucket
-resource "aws_s3_bucket_notification" "config_lambda_notification" {
-  # For now, only create a trigger in internal-dev and prod as those are the envs with a config bucket
-  count = local.create_config_bucket ? 1 : 0
-
-  bucket = aws_s3_bucket.batch_config_bucket[0].bucket
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.redis_sync_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-}
-
-# Permission for the new S3 bucket to invoke the Lambda function
-resource "aws_lambda_permission" "new_s3_invoke_permission" {
-  count = local.create_config_bucket ? 1 : 0
-
-  statement_id  = "AllowExecutionFromNewS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.redis_sync_lambda.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = local.config_bucket_arn
-}
-
-# IAM Role for ElastiCache.
-resource "aws_iam_role" "elasticache_exec_role" {
-  name = "${local.short_prefix}-elasticache-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Sid    = "",
-      Principal = {
-        Service = "elasticache.amazonaws.com" # ElastiCache service principal
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+#
+# # IAM Role for ElastiCache.
+# resource "aws_iam_role" "elasticache_exec_role" {
+#   name = "${local.short_prefix}-elasticache-exec-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [{
+#       Effect = "Allow",
+#       Sid    = "",
+#       Principal = {
+#         Service = "elasticache.amazonaws.com" # ElastiCache service principal
+#       },
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
 resource "aws_iam_policy" "elasticache_permissions" {
   name = "${local.short_prefix}-elasticache-permissions"
@@ -312,13 +272,82 @@ resource "aws_iam_policy" "elasticache_permissions" {
   })
 }
 
+# Attach the execution policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "redis_sync_lambda_exec_policy_attachment" {
+  role       = aws_iam_role.redis_sync_lambda_exec_role.name
+  policy_arn = aws_iam_policy.redis_sync_lambda_exec_policy.arn
+}
+
+# Attach the kms policy to the Lambda role
+resource "aws_iam_role_policy_attachment" "redis_sync_lambda_kms_policy_attachment" {
+  role       = aws_iam_role.redis_sync_lambda_exec_role.name
+  policy_arn = aws_iam_policy.redis_sync_lambda_kms_access_policy.arn
+}
+
 # Attach the policy to the ElastiCache role
 resource "aws_iam_role_policy_attachment" "elasticache_policy_attachment" {
-  role       = aws_iam_role.elasticache_exec_role.name
+  # role       = aws_iam_role.elasticache_exec_role.name
+  role       = aws_iam_role.redis_sync_lambda_exec_role.name
   policy_arn = aws_iam_policy.elasticache_permissions.arn
 }
 
-resource "aws_cloudwatch_log_group" "redis_sync_lambda_log_group" {
-  name              = local.redis_cloudwatch_group_name
+# Lambda Function with Security Group and VPC.
+resource "aws_lambda_function" "redis_sync_lambda" {
+  function_name = "${local.short_prefix}-redis_sync_lambda"
+  role          = aws_iam_role.redis_sync_lambda_exec_role.arn
+  package_type  = "Image"
+  image_uri     = module.redis_sync_docker_image.image_uri
+  architectures = ["x86_64"]
+  timeout       = 360
+
+  vpc_config {
+    subnet_ids         = data.aws_subnets.default.ids
+    security_group_ids = [data.aws_security_group.existing_securitygroup.id]
+  }
+
+  environment {
+    variables = {
+      CONFIG_BUCKET_NAME          = local.config_bucket_name
+      REDIS_HOST                  = data.aws_elasticache_cluster.existing_redis.cache_nodes[0].address
+      REDIS_PORT                  = data.aws_elasticache_cluster.existing_redis.cache_nodes[0].port
+      REDIS_SYNC_PROC_LAMBDA_NAME = "imms-${local.env}-redis_sync_lambda"
+      # SPLUNK_FIREHOSE_NAME        = module.splunk.firehose_stream_name
+    }
+  }
+  kms_key_arn = data.aws_kms_key.existing_lambda_encryption_key.arn
+
+  depends_on = [
+    aws_cloudwatch_log_group.redis_sync_log_group,
+    aws_iam_policy.redis_sync_lambda_exec_policy
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "redis_sync_log_group" {
+  name              = "/aws/lambda/${local.short_prefix}-redis_sync_lambda"
   retention_in_days = 30
+}
+
+
+# S3 Bucket notification to trigger Lambda function for config bucket
+resource "aws_s3_bucket_notification" "config_lambda_notification" {
+  # For now, only create a trigger in internal-dev and prod as those are the envs with a config bucket
+  count = local.create_config_bucket ? 1 : 0
+
+  bucket = aws_s3_bucket.batch_config_bucket[0].bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.redis_sync_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+# Permission for the new S3 bucket to invoke the Lambda function
+resource "aws_lambda_permission" "new_s3_invoke_permission" {
+  count = local.create_config_bucket ? 1 : 0
+
+  statement_id  = "AllowExecutionFromNewS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.redis_sync_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = local.config_bucket_arn
 }
