8e283b871767082d9d5b308ab40758d0b679accd
nhsdevws <stephen.wates1@nhs.net>
2025-06-20 11:29:35 +0100

    key diseases_to_vacc

--- redis_sync/tests/test_transform_vaccine_map.py
+++ redis_sync/tests/test_transform_vaccine_map.py
@@ -22,20 +22,20 @@ class TestTransformVaccineMap(unittest.TestCase):
         result = transform_vaccine_map(sample_map)
         # Check that all vaccine keys are present and map to the correct diseases
         for entry in sample_map:
-            self.assertIn(entry["vacc_type"], result["vaccine"])
-            self.assertEqual(result["vaccine"][entry["vacc_type"]], entry["diseases"])
+            self.assertIn(entry["vacc_type"], result["vacc_to_diseases"])
+            self.assertEqual(result["vacc_to_diseases"][entry["vacc_type"]], entry["diseases"])
         # Check that all disease keys are present and map to the correct vaccine
         for entry in sample_map:
             disease_codes = ':'.join(sorted(d["code"] for d in entry["diseases"]))
-            self.assertIn(disease_codes, result["disease"])
-            self.assertEqual(result["disease"][disease_codes], entry["vacc_type"])
+            self.assertIn(disease_codes, result["diseases_to_vacc"])
+            self.assertEqual(result["diseases_to_vacc"][disease_codes], entry["vacc_type"])
 
     def test_disease_to_vacc(self):
         """ Test that the disease to vaccine mapping is correct."""
         with open("./tests/test_data/expected_disease_to_vacc.json") as f:
             expected_disease_to_vacc = json.load(f)
             result = transform_vaccine_map(sample_map)
-            self.assertEqual(result["disease"], expected_disease_to_vacc)
+            self.assertEqual(result["diseases_to_vacc"], expected_disease_to_vacc)
 
     def test_vacc_to_diseases(self):
 
@@ -43,4 +43,4 @@ class TestTransformVaccineMap(unittest.TestCase):
             expected_vacc_to_diseases = json.load(f)
 
             result = transform_vaccine_map(sample_map)
-            self.assertEqual(result["vaccine"], expected_vacc_to_diseases)
+            self.assertEqual(result["vacc_to_diseases"], expected_vacc_to_diseases)
