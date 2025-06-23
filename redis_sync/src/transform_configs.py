cb1b81782b5760ad31114d5a83e90d1f7577b082
Akol125 <akinola.olutola1@nhs.net>
2025-06-23 12:49:47 +0100

    sync-permissions

--- redis_sync/src/transform_configs.py
+++ redis_sync/src/transform_configs.py
@@ -13,3 +13,18 @@ def transform_vaccine_map(map):
         "vacc_to_diseases": vacc_to_diseases,
         "diseases_to_vacc": diseases_to_vacc
     }
+
+
+def transform_supplier_permissions(supplier):
+    """
+    Transforms a list of supplier-permission entries into a mapping of:
+    supplier -> list of permissions
+    """
+    logger.info("Transforming supplier permissions data")
+    logger.info("source data: %s", supplier)
+
+    supplier_permissions = {
+        entry["supplier"]: entry["permissions"] for entry in supplier
+    }
+
+    return supplier_permissions
