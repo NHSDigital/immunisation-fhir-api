3f6fba26963122290b4d240987498222ed572bc9
robertnovac1 <196046006+robertnovac1@users.noreply.github.com>
2025-06-25 17:41:01 +0100

    Included old terraform folder that creates the lambdas so we can have recreate blue/green env in preprod

---
+++ terraform_old/variables.tf
@@ -0,0 +1,109 @@
+variable "project_name" {
+    default = "immunisations"
+}
+
+variable "project_short_name" {
+    default = "imms"
+}
+
+variable "service" {
+    default = "fhir-api"
+}
+data "aws_vpc" "default" {
+    default = true
+}
+data "aws_subnets" "default" {
+    filter {
+        name   = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+locals {
+    root_domain = "${local.config_env}.vds.platform.nhs.uk"
+}
+
+locals {
+    project_domain_name = data.aws_route53_zone.project_zone.name
+}
+
+locals {
+    environment         = terraform.workspace == "green" ? "prod" : terraform.workspace == "blue" ? "prod" : terraform.workspace
+    env                 = terraform.workspace
+    prefix              = "${var.project_name}-${var.service}-${local.env}"
+    short_prefix        = "${var.project_short_name}-${local.env}"
+    batch_prefix        = "immunisation-batch-${local.env}"
+    service_domain_name = "${local.env}.${local.project_domain_name}"
+    config_env = local.environment == "prod" ? "prod" : "dev"
+    config_bucket_env = local.environment == "prod" ? "prod" : "internal-dev"
+
+    tags = {
+        Project     = var.project_name
+        Environment = local.environment
+        Service     = var.service
+    }
+}
+
+variable "region" {
+    default = "eu-west-2"
+}
+
+data "aws_kms_key" "existing_s3_encryption_key" {
+  key_id = "alias/imms-batch-s3-shared-key"
+}
+
+data "aws_kms_key" "existing_dynamo_encryption_key" {
+  key_id = "alias/imms-event-dynamodb-encryption"
+}
+
+variable "aws_region" {
+    default = "eu-west-2"
+}
+
+data "aws_elasticache_cluster" "existing_redis" {
+  cluster_id = "immunisation-redis-cluster"
+}
+
+data "aws_security_group" "existing_securitygroup" {
+  filter {
+    name   = "group-name"
+    values = ["immunisation-security-group"]
+  }
+}
+
+data "aws_s3_bucket" "existing_config_bucket" {
+  bucket = "imms-${local.config_bucket_env}-supplier-config"
+}
+
+data "aws_s3_bucket" "existing_destination_bucket" {
+  bucket = "immunisation-batch-${local.local_config}-data-destinations"
+}
+
+data "aws_s3_bucket" "existing_source_bucket" {
+  bucket = "immunisation-batch-${local.local_config}-data-sources"
+}
+
+data "aws_kms_key" "existing_lambda_encryption_key" {
+  key_id = "alias/imms-batch-lambda-env-encryption"
+}
+
+data "aws_kms_key" "existing_kinesis_encryption_key" {
+  key_id = "alias/imms-batch-kinesis-stream-encryption"
+}
+
+data "aws_dynamodb_table" "events-dynamodb-table" { 
+  name = "imms-${local.local_config}-imms-events" 
+}
+
+data "aws_dynamodb_table" "audit-table" { 
+  name = "immunisation-batch-${local.local_config}-audit-table" 
+}
+
+data "aws_dynamodb_table" "delta-dynamodb-table" { 
+  name = "imms-${local.local_config}-delta" 
+}
+
+data "aws_lambda_function" "existing_file_name_proc_lambda" {
+  function_name = aws_lambda_function.file_processor_lambda.function_name
+}
+
