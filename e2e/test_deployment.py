9aa584e0ed5e6786dd2d85b06fedc331bd0bf4a5
nhsdevws <stephen.wates1@nhs.net>
2025-07-07 10:10:36 +0100

    e2e test

--- e2e/test_deployment.py
+++ e2e/test_deployment.py
@@ -11,7 +11,6 @@ deployment with the one that returned from the deployed proxy.
 You can ignore these tests if you are running them in your local environment"""
 
 
-@unittest.skip("This test is skipped because it requires a live FHIR server connection.")
 class TestDeployment(unittest.TestCase):
     proxy_url: str
     status_api_key: str
