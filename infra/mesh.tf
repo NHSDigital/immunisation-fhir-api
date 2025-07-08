56e9608a19e027c8cb15110f4da6a3269f2d9c51
robertnovac1 <196046006+robertnovac1@users.noreply.github.com>
2025-07-08 10:36:08 +0100

    Infra imported resources into state

---
+++ infra/mesh.tf
@@ -0,0 +1,12 @@
+module "mesh" {
+  source                      = "git::https://github.com/nhsdigital/terraform-aws-mesh-client.git//module?ref=v2.1.5"
+  name_prefix                 = "local-immunisation"
+  mesh_env                    = "integration"
+  subnet_ids                  = aws_subnets.default.ids
+  mailbox_ids                 = ["X26OT303"] #TBC 
+  verify_ssl                  = "true"
+  get_message_max_concurrency = 10
+  compress_threshold          = 1 * 1024 * 1024
+  handshake_schedule          = "rate(24 hours)"
+  account_id                  = var.imms_account_id
+}
