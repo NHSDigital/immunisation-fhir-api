f3bc62444555e61ba481564b267945a07659c048
Matt Jarvis <matt.jarvis2@nhs.net>
2025-07-09 13:25:06 +0100

    VED-26: Remove unused AWS clients.

--- backend/src/aws_clients.py
+++ backend/src/aws_clients.py
@@ -4,8 +4,4 @@ import os
 
 REGION_NAME = os.getenv("AWS_REGION", "eu-west-2")
 
-s3_client = boto3_client("s3", region_name=REGION_NAME)
-kinesis_client = boto3_client("kinesis", region_name=REGION_NAME)
-lambda_client = boto3_client("lambda", region_name=REGION_NAME)
-firehose_client = boto3_client("firehose", region_name=REGION_NAME)
 sqs_client = boto3_client("sqs", region_name=REGION_NAME)
