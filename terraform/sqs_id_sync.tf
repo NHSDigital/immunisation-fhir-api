f3b99868faad0d4471fc2243c29ff8cbae931790
James Wharmby <james.wharmby1@nhs.net>
2025-07-17 09:35:07 +0100

    reworked to standard Q with DLQ

--- terraform/sqs_id_sync.tf
+++ terraform/sqs_id_sync.tf
@@ -1,7 +1,24 @@
-# FIFO SQS Queue
+# Standard SQS Queue
+
 resource "aws_sqs_queue" "id_sync_queue" {
-  name                        = "${local.short_prefix}-id-sync-queue.fifo" # Must end with .fifo
-  fifo_queue                  = true
-  content_based_deduplication = true # Optional, helps with deduplication
+  name                        = "${local.short_prefix}-id-sync-queue"
   visibility_timeout_seconds  = 60
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.id_sync_dlq.arn
+    maxReceiveCount     = 4
+  })}
+
+# DLQ for id-sync-queue
+
+resource "aws_sqs_queue" "id_sync_dlq" {
+  name = "${local.short_prefix}-id-sync-dlq"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "id_sync_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.id_sync_dlq.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.id_sync_queue.arn]
+  })
 }
