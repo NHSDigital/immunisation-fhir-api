f7a2d75d622259ccb6bfc53f1fa1f28afa7b16da
nhsdevws <stephen.wates1@nhs.net>
2025-07-18 11:28:34 +0100

    id sync path

--- lambdas/id_sync/tests/test_record_processor.py
+++ lambdas/id_sync/tests/test_record_processor.py
@@ -8,7 +8,11 @@ class TestRecordProcessor(unittest.TestCase):
         self.logger_info_patcher = patch("logging.Logger.info")
         self.mock_logger_info = self.logger_info_patcher.start()
 
+    def tearDown(self):
+        patch.stopall()
+
     def test_record_processor_success(self):
         test_record = "abc1"
-        response = process_record(test_record, None)
+        response = 
+        (test_record, None)
         self.assertEqual(response, f"hello world {test_record}")
