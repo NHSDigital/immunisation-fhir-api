bc306e69ead63e262e9d8b482374ee861ee7f0ab
James Wharmby <james.wharmby1@nhs.net>
2025-07-22 11:09:22 +0100

    fix: existing_id_sync_sqs_encryption_key

--- terraform/sqs_id_sync.tf
+++ terraform/sqs_id_sync.tf
@@ -2,7 +2,7 @@
 
 resource "aws_sqs_queue" "id_sync_queue" {
   name                        = "${local.short_prefix}-id-sync-queue"
-  kms_master_key_id           = aws_kms_alias.id_sync_sqs_encryption.name
+  kms_master_key_id           = data.aws_kms_key.existing_id_sync_sqs_encryption_key.arn
   visibility_timeout_seconds  = 60
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.id_sync_dlq.arn
