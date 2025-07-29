d919f6ffec476a7b986f92adc261c9320d5897e6
nhsdevws <stephen.wates1@nhs.net>
2025-07-29 17:33:32 +0100

    safe_tmp_dir

--- lambdas/id_sync/src/pds_details.py
+++ lambdas/id_sync/src/pds_details.py
@@ -1,6 +1,7 @@
 '''
     record Processor
 '''
+import tempfile
 from common.clients import logger, secrets_manager_client
 from common.cache import Cache
 from os_vars import get_pds_env
@@ -8,13 +9,14 @@ from common.pds_service import PdsService
 from common.authentication import AppRestrictedAuth, Service
 
 pds_env = get_pds_env()
+safe_tmp_dir = tempfile.mkdtemp(dir="/tmp")
 
 
 def pds_get_patient_details(nhs_number: str) -> dict:
     try:
         logger.info(f"Get PDS patient details for {nhs_number}")
 
-        cache = Cache(directory="/tmp")
+        cache = Cache(directory=safe_tmp_dir)
         authenticator = AppRestrictedAuth(
             service=Service.PDS,
             secret_manager_client=secrets_manager_client,
