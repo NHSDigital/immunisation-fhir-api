3c8b215bd3a583568cbd483ab7ea732612705aaf
robertnovac1 <196046006+robertnovac1@users.noreply.github.com>
2025-08-04 10:30:50 +0100

    Batch - ACK fix

--- ack_backend/tests/utils_for_ack_backend_tests/mock_environment_variables.py
+++ ack_backend/tests/utils_for_ack_backend_tests/mock_environment_variables.py
@@ -21,5 +21,5 @@ MOCK_ENVIRONMENT_DICT = {
     "ACK_BUCKET_NAME": BucketNames.DESTINATION,
     "FIREHOSE_STREAM_NAME": Firehose.STREAM_NAME,
     "AUDIT_TABLE_NAME": "immunisation-batch-internal-dev-audit-table",
-    "ENVIRONMENT": "internal-dev",
+    "SOURCE_BUCKET_NAME": BucketNames.SOURCE
 }
