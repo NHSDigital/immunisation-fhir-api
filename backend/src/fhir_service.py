7e27b617ff9c90749cc8f31d412f3b36e38d5a23
nhsdevws <stephen.wates1@nhs.net>
2025-08-08 09:32:51 +0100

    debug

--- backend/src/fhir_service.py
+++ backend/src/fhir_service.py
@@ -319,7 +319,6 @@ class FhirService:
         logger.info("SAW: filter resources for search")
         # log pretty resources for debug
         logger.debug("SAW: resources: %s", [r.json(indent=2) for r in resources])
-        logger.info("SAW: filtered resources: %d", len(resources_filtered_for_search))
         # Filter and amend the immunization resources for the SEARCH response
         resources_filtered_for_search = [Filter.search(imms, patient_full_url) for imms in resources]
         logger.info("SAW: no of items in filtered resources: %d", len(resources_filtered_for_search))
