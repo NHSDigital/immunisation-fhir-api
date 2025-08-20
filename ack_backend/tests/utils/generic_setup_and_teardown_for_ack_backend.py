93a647a07ece9bb8c3db29ff31490ee37d980aac
James Wharmby <james.wharmby1@nhs.net>
2025-08-20 14:06:19 +0100

    unit tests, mock dynamodb, refactoring II

--- ack_backend/tests/utils/generic_setup_and_teardown_for_ack_backend.py
+++ ack_backend/tests/utils/generic_setup_and_teardown_for_ack_backend.py
@@ -1,6 +1,13 @@
 """Generic setup and teardown for ACK backend tests"""
 
-from tests.utils.mock_environment_variables import BucketNames, Firehose, REGION_NAME
+from unittest.mock import patch
+
+from tests.utils.mock_environment_variables import BucketNames, MOCK_ENVIRONMENT_DICT, Firehose
+
+# Ensure environment variables are mocked before importing from src files
+with patch.dict("os.environ", MOCK_ENVIRONMENT_DICT):
+    from clients import REGION_NAME
+    from constants import AuditTableKeys, AUDIT_TABLE_NAME, AUDIT_TABLE_QUEUE_NAME_GSI, AUDIT_TABLE_FILENAME_GSI
 
 
 class GenericSetUp:
@@ -11,7 +18,7 @@ class GenericSetUp:
     * If firehose_client is provided, creates a firehose delivery stream
     """
 
-    def __init__(self, s3_client=None, firehose_client=None):
+    def __init__(self, s3_client=None, firehose_client=None, dynamodb_client=None):
 
         if s3_client:
             for bucket_name in [BucketNames.SOURCE, BucketNames.DESTINATION, BucketNames.MOCK_FIREHOSE]:
@@ -30,11 +37,41 @@ class GenericSetUp:
                 },
             )
 
+        if dynamodb_client:
+            dynamodb_client.create_table(
+                TableName=AUDIT_TABLE_NAME,
+                KeySchema=[{"AttributeName": AuditTableKeys.MESSAGE_ID, "KeyType": "HASH"}],
+                AttributeDefinitions=[
+                    {"AttributeName": AuditTableKeys.MESSAGE_ID, "AttributeType": "S"},
+                    {"AttributeName": AuditTableKeys.FILENAME, "AttributeType": "S"},
+                    {"AttributeName": AuditTableKeys.QUEUE_NAME, "AttributeType": "S"},
+                    {"AttributeName": AuditTableKeys.STATUS, "AttributeType": "S"},
+                ],
+                ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+                GlobalSecondaryIndexes=[
+                    {
+                        "IndexName": AUDIT_TABLE_FILENAME_GSI,
+                        "KeySchema": [{"AttributeName": AuditTableKeys.FILENAME, "KeyType": "HASH"}],
+                        "Projection": {"ProjectionType": "KEYS_ONLY"},
+                        "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+                    },
+                    {
+                        "IndexName": AUDIT_TABLE_QUEUE_NAME_GSI,
+                        "KeySchema": [
+                            {"AttributeName": AuditTableKeys.QUEUE_NAME, "KeyType": "HASH"},
+                            {"AttributeName": AuditTableKeys.STATUS, "KeyType": "RANGE"},
+                        ],
+                        "Projection": {"ProjectionType": "ALL"},
+                        "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+                    },
+                ],
+            )
+
 
 class GenericTearDown:
     """Performs generic tear down of mock resources"""
 
-    def __init__(self, s3_client=None, firehose_client=None):
+    def __init__(self, s3_client=None, firehose_client=None, dynamodb_client=None):
 
         if s3_client:
             for bucket_name in [BucketNames.SOURCE, BucketNames.DESTINATION, BucketNames.MOCK_FIREHOSE]:
@@ -44,3 +81,6 @@ class GenericTearDown:
 
         if firehose_client:
             firehose_client.delete_delivery_stream(DeliveryStreamName=Firehose.STREAM_NAME)
+
+        if dynamodb_client:
+            dynamodb_client.delete_table(TableName=AUDIT_TABLE_NAME)
