b0a4333bebf83a029375692c6bf0c9113e16f857
Daniel Yip <daniel.yip4@nhs.net>
2025-08-20 16:31:00 +0100

    Fix visibility timeout parameter

--- terraform/sqs_batch_file_created.tf
+++ terraform/sqs_batch_file_created.tf
@@ -20,5 +20,5 @@ resource "aws_sqs_queue" "batch_file_created_queue" {
 	policy                      = data.aws_iam_policy_document.batch_file_created_queue_policy.json
   fifo_queue                  = true
   content_based_deduplication = true # Optional, helps with deduplication
-  visibility_timeout_seconds  = 60
+  visibility_timeout_seconds  = 420
 }
