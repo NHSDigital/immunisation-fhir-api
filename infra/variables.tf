1c59b08a1323f8c89990da4c099a51ac8f38b236
Matt Jarvis <61870561+mfjarvis@users.noreply.github.com>
2025-08-22 15:33:07 +0100

    VED-711: Terraform changes for preprod account (#756)
    * Use consistent naming for dev / preprod / prod accounts. Rename int blue / green environments to int-blue / int-green. Update pipelines to supply the correct parameters. TF fixes for state import.
    
    * Look up VPC by name. Disable MESH in preprod. Use correct cert in preprod. Remove unused vars.
    
    * Revert admin role change as this prevents auto-ops from managing KMS keys via TF.
    
    * Change back to apply.
    
    * Add blue deploy job. Fix e2e test condition.
    
    * Add import command
    
    * Disable MESH processor in prod.
    
    * Change back to apply. Add deletion protection to all DDB tables.
    
    * Pin specific commits for external GitHub actions.
    
    * Revert change made for local use.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,42 @@
 variable "aws_region" {
+  type    = string
   default = "eu-west-2"
 }
-
-variable "imms_account_id" {}
-variable "dspp_account_id" {}
-variable "auto_ops_role" {}
-variable "admin_role" {}
-variable "dev_ops_role" {}
-variable "dspp_admin_role" {}
-variable "parent_route53_zone_name" {}
-variable "child_route53_zone_name" {}
+variable "imms_account_id" {
+  description = "Immunisation AWS account ID"
+  type        = string
+}
+variable "dspp_account_id" {
+  description = "DSPP Core AWS account ID"
+  type        = string
+}
+variable "auto_ops_role" {
+  default = "role/auto-ops"
+  type    = string
+}
+variable "admin_role" {
+  default = "root"
+  type    = string
+}
+variable "dev_ops_role" {
+  type = string
+}
+variable "dspp_admin_role" {
+  type = string
+}
 variable "build_agent_account_id" {
+  type    = string
   default = "958002497996"
 }
 variable "environment" {
-  default = "non-prod"
+  type        = string
+  description = "Immunisation AWS account name (dev / preprod / prod)"
+  default     = "dev"
+}
+variable "blue_green_split" {
+  type        = bool
+  description = "Whether this account uses blue / green split deployments"
+  default     = false
 }
 
 variable "mns_account_id" {}
