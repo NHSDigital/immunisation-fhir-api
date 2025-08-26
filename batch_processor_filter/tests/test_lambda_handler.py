b7c7d059c7e9ceff9bfbd9658b4eeaf2157c98aa
Daniel Yip <daniel.yip4@nhs.net>
2025-08-26 12:12:51 +0100

    Minor fixes

--- batch_processor_filter/tests/test_lambda_handler.py
+++ batch_processor_filter/tests/test_lambda_handler.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from lambda_handler import lambda_handler
 
+
 class TestLambdaHandler(TestCase):
     def test_lambda_handler(self):
         result = lambda_handler({}, {})
