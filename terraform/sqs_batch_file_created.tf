c2161def67f2f4a4b63a2694eaad3bec5a12adb4
Daniel Yip <daniel.yip4@nhs.net>
2025-08-26 12:12:51 +0100

    Minor fixes

--- terraform/sqs_batch_file_created.tf
+++ terraform/sqs_batch_file_created.tf
@@ -1,7 +1,6 @@
 # FIFO SQS Queue - targeted by Filename Processor Lambda function
 resource "aws_sqs_queue" "batch_file_created" {
   name                        = "${local.short_prefix}-batch-file-created-queue.fifo"
-  policy                      = data.aws_iam_policy_document.batch_file_created_queue_policy.json
   fifo_queue                  = true
   content_based_deduplication = true # Optional, helps with deduplication
   visibility_timeout_seconds  = 900 # TODO - discuss and refine both this, max receives and DLQ
