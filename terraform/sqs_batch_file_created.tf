8c3f4a9f860b366d6dd89169963433bb8f08de09
Daniel Yip <daniel.yip4@nhs.net>
2025-08-29 10:33:08 +0100

    Removed todo following discussion

--- terraform/sqs_batch_file_created.tf
+++ terraform/sqs_batch_file_created.tf
@@ -3,5 +3,5 @@ resource "aws_sqs_queue" "batch_file_created" {
   name                        = "${local.short_prefix}-batch-file-created-queue.fifo"
   fifo_queue                  = true
   content_based_deduplication = true # Optional, helps with deduplication
-  visibility_timeout_seconds  = 60 # TODO - discuss and refine this + DDB TTL
+  visibility_timeout_seconds  = 60
 }
