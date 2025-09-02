192946c184859a9ddfd3f2a5a81d9c34863f943b
James Wharmby <james.wharmby1@nhs.net>
2025-09-02 13:59:46 +0100

    mock DELTA_TTL II

--- delta_backend/tests/test_convert.py
+++ delta_backend/tests/test_convert.py
@@ -11,6 +11,7 @@ from common.mappings import ActionFlag, Operation, EventName
 MOCK_ENV_VARS = {
     "AWS_SQS_QUEUE_URL": "https://sqs.eu-west-2.amazonaws.com/123456789012/test-queue",
     "DELTA_TABLE_NAME": "immunisation-batch-internal-dev-audit-test-table",
+    "DELTA_TTL": "14",
     "SOURCE": "test-source",
 }
 request_json_data = ValuesForTests.json_data
