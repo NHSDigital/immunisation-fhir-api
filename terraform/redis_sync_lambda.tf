a4a36a8f5679138397485f45212caae88e68cd18
James Wharmby <james.wharmby1@nhs.net>
2025-09-08 13:04:28 +0100

    redis_sync_lambda.tf

--- terraform/redis_sync_lambda.tf
+++ terraform/redis_sync_lambda.tf
@@ -20,7 +20,7 @@ resource "aws_ecr_repository" "redis_sync_lambda_repository" {
 module "redis_sync_docker_image" {
   source           = "terraform-aws-modules/lambda/aws//modules/docker-build"
   version          = "8.0.1"
-  docker_file_path = "./redis_sync/Dockerfile"
+
   create_ecr_repo  = false
   ecr_repo         = aws_ecr_repository.redis_sync_lambda_repository.name
   ecr_repo_lifecycle_policy = jsonencode({
