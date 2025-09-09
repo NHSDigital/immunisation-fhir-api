864259f9248d2e346e1eec27f813bce78e81aebd
Daniel Yip <daniel.yip4@nhs.net>
2025-09-09 13:53:31 +0100

    Terraform errors for chatbot

--- terraform/batch_processor_errors_slack_chatbot.tf
+++ terraform/batch_processor_errors_slack_chatbot.tf
@@ -1,24 +1,24 @@
-resource "awscc_chatbot_slack_channel_configuration" "batch_processor_errors" {
+resource "aws_chatbot_slack_channel_configuration" "batch_processor_errors" {
   count              = var.batch_error_notifications_enabled ? 1 : 0
 
   configuration_name = "${local.resource_scope}-batch-processor-errors-slack-channel-config"
-  iam_role_arn       = awscc_iam_role.batch_processor_errors_chatbot[0].arn
-  slack_channel_id   = var.environment == "prod" ? "TODO - make channel" : "C09E48NDP18"
-  slack_workspace_id = "TJ00QR03U"
+  iam_role_arn       = aws_iam_role.batch_processor_errors_chatbot[0].arn
+  slack_channel_id   = var.environment == "prod" ? "C09EA0HE202" : "C09E48NDP18"
+  slack_team_id      = "TJ00QR03U"
   sns_topic_arns     = [aws_sns_topic.batch_processor_errors[0].arn]
 }
 
-resource "awscc_iam_role" "batch_processor_errors_chatbot" {
+resource "aws_iam_role" "batch_processor_errors_chatbot" {
   count = var.batch_error_notifications_enabled ? 1 : 0
 
-  role_name = "${local.resource_scope}-batch-processor-errors-chatbot-channel-role"
-  assume_role_policy_document = jsonencode({
+  name               = "${local.resource_scope}-batch-processor-errors-chatbot-channel-role"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
-        Sid    = ""
+        Sid    = "AssumeChatbotRole"
         Principal = {
           Service = "chatbot.amazonaws.com"
         }
