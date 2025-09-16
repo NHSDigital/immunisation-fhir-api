f55a9a5b4a8f015da4c1d46ff33da428f1ca5271
James Wharmby <james.wharmby1@nhs.net>
2025-09-16 09:22:41 +0100

    errors III

--- lambdas/shared/src/common/models/errors.py
+++ lambdas/shared/src/common/models/errors.py
@@ -12,8 +12,8 @@ class Severity(str, Enum):
 class Code(str, Enum):
     forbidden = "forbidden"
     not_found = "not-found"
-    invalid = "invalid"
-    server_error = "exception"
+    invalid = "invalid or missing access token"
+    server_error = "internal server error"
     invariant = "invariant"
     incomplete = "parameter-incomplete"
     not_supported = "not-supported"
@@ -43,6 +43,12 @@ class UnauthorizedError(RuntimeError):
 
 @dataclass
 class UnauthorizedVaxError(RuntimeError):
+    response: dict | str
+    message: str
+
+    def __str__(self):
+        return f"{self.message}\n{self.response}"
+
     @staticmethod
     def to_operation_outcome() -> dict:
         msg = "Unauthorized request for vaccine type"
@@ -56,6 +62,12 @@ class UnauthorizedVaxError(RuntimeError):
 
 @dataclass
 class UnauthorizedVaxOnRecordError(RuntimeError):
+    response: dict | str
+    message: str
+
+    def __str__(self):
+        return f"{self.message}\n{self.response}"
+
     @staticmethod
     def to_operation_outcome() -> dict:
         msg = "Unauthorized request for vaccine type present in the stored immunization resource"
@@ -109,7 +121,7 @@ class ConflictError(RuntimeError):
 class ResourceNotFoundError(RuntimeError):
     """Return this error when the requested FHIR resource does not exist"""
 
-    resource_type: str
+    resource_type: str | None
     resource_id: str
 
     def __str__(self):
@@ -284,30 +296,6 @@ class ServerError(RuntimeError):
         )
 
 
-def create_operation_outcome(resource_id: str, severity: Severity, code: Code, diagnostics: str) -> dict:
-    """Create an OperationOutcome object. Do not use `fhir.resource` library since it adds unnecessary validations"""
-    return {
-        "resourceType": "OperationOutcome",
-        "id": resource_id,
-        "meta": {"profile": ["https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"]},
-        "issue": [
-            {
-                "severity": severity,
-                "code": code,
-                "details": {
-                    "coding": [
-                        {
-                            "system": "https://fhir.nhs.uk/Codesystem/http-error-codes",
-                            "code": code.upper(),
-                        }
-                    ]
-                },
-                "diagnostics": diagnostics,
-            }
-        ],
-    }
-
-
 @dataclass
 class ParameterException(RuntimeError):
     message: str
@@ -349,3 +337,27 @@ class RecordProcessorError(Exception):
 
     def __init__(self, diagnostics_dictionary: dict):
         self.diagnostics_dictionary = diagnostics_dictionary
+
+
+def create_operation_outcome(resource_id: str, severity: Severity, code: Code, diagnostics: str) -> dict:
+    """Create an OperationOutcome object. Do not use `fhir.resource` library since it adds unnecessary validations"""
+    return {
+        "resourceType": "OperationOutcome",
+        "id": resource_id,
+        "meta": {"profile": ["https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"]},
+        "issue": [
+            {
+                "severity": severity,
+                "code": code,
+                "details": {
+                    "coding": [
+                        {
+                            "system": "https://fhir.nhs.uk/Codesystem/http-error-codes",
+                            "code": code.upper(),
+                        }
+                    ]
+                },
+                "diagnostics": diagnostics,
+            }
+        ],
+    }
