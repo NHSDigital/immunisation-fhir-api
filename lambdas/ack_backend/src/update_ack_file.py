aebd613963242106848fe99036af0427ffcfb54b
James Wharmby <james.wharmby1@nhs.net>
2025-09-17 16:56:50 +0100

    imports

--- lambdas/ack_backend/src/update_ack_file.py
+++ lambdas/ack_backend/src/update_ack_file.py
@@ -1,13 +1,13 @@
 """Functions for uploading the data to the ack file"""
 
+from botocore.exceptions import ClientError
 from io import StringIO, BytesIO
 from typing import Optional
-from botocore.exceptions import ClientError
-from constants import ACK_HEADERS, get_source_bucket_name, get_ack_bucket_name
 from audit_table import change_audit_table_status_to_processed
 from common.clients import get_s3_client, logger
-from utils_for_ack_lambda import get_row_count
+from constants import ACK_HEADERS, get_source_bucket_name, get_ack_bucket_name
 from logging_decorators import upload_ack_file_logging_decorator
+from utils_for_ack_lambda import get_row_count
 
 
 def create_ack_data(
