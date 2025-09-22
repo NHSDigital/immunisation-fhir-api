ed1915f2efd80c66074b3142cd7ba169f89ad9e2
James Wharmby <james.wharmby1@nhs.net>
2025-09-22 08:57:08 +0100

    remove duplication

--- lambdas/shared/src/common/redis_client.py
+++ lambdas/shared/src/common/redis_client.py
@@ -2,11 +2,6 @@ import os
 import redis
 from common.clients import logger
 
-STREAM_NAME = os.getenv("SPLUNK_FIREHOSE_NAME", "firehose-name-not-defined")
-CONFIG_BUCKET_NAME = os.getenv("CONFIG_BUCKET_NAME", "variconfig-bucketable-not-defined")
-
-REGION_NAME = os.getenv("AWS_REGION", "eu-west-2")
-
 REDIS_HOST = os.getenv("REDIS_HOST", "")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 
