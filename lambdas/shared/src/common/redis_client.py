9fe7761cb56efb2cd899196fef6f6ca648d0b751
nhsdevws <stephen.wates1@nhs.net>
2025-10-02 15:14:44 +0100

    debug

--- lambdas/shared/src/common/redis_client.py
+++ lambdas/shared/src/common/redis_client.py
@@ -11,6 +11,7 @@ redis_client = None
 def get_redis_client():
     global redis_client
     if redis_client is None:
+        print("SAW: get_redis_client")
         logger.info(f"Connecting to Redis at {REDIS_HOST}:{REDIS_PORT}")
         redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
     return redis_client
