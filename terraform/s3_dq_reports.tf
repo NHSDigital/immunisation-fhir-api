60db91d33462c50d0f5ddd3c27113d2a5ab5b687
Akol125 <akinola.olutola1@nhs.net>
2025-10-08 14:37:52 +0100

    add s3 lifecycle and policies

--- terraform/s3_dq_reports.tf
+++ terraform/s3_dq_reports.tf
@@ -15,10 +15,45 @@ resource "aws_s3_bucket_public_access_block" "data_quality_reports_bucket_public
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "data_destinations" {
+  bucket = aws_s3_bucket.data_quality_reports_bucket.id
 
+  rule {
+    id     = "DeleteFilesFromForwardedFile"
+    status = "Enabled"
+
+    filter {
+    }
+
+    expiration {
+      days = 14
+    }
+  }
+}
+
+
+# Add versioning to prevent against accidental deletes
 resource "aws_s3_bucket_versioning" "dq_source_versioning" {
   bucket = aws_s3_bucket.data_quality_reports_bucket.bucket
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+
+# If used should attached to lambda or any aws service that needs to perform any operation
+resource "aws_iam_policy" "s3_dq_access" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+        Resource = [
+          aws_s3_bucket.data_quality_reports_bucket.arn,
+          "${aws_s3_bucket.data_quality_reports_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
