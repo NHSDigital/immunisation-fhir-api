b5068e9dcd4ac7e62ab8b38ee24ae886d7ac26ba
Akol125 <akinola.olutola1@nhs.net>
2025-10-14 13:48:13 +0100

    VED-863: Attach Policy for kms and s3 to ecs task policy

--- terraform/ecs_batch_processor_config.tf
+++ terraform/ecs_batch_processor_config.tf
@@ -157,6 +157,22 @@ resource "aws_iam_policy" "ecs_task_exec_policy" {
           "firehose:PutRecordBatch"
         ],
         "Resource" : "arn:aws:firehose:*:*:deliverystream/${module.splunk.firehose_stream_name}"
+      },
+      {
+       "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+        ],
+        "Resource" : "${aws_s3_bucket.data_quality_reports_bucket.arn}/*"
+      },
+      {
+       "Effect" = "Allow",
+        "Action" = [
+          "kms:Encrypt",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource = aws_kms_key.s3_shared_key.arn
       }
     ]
   })
