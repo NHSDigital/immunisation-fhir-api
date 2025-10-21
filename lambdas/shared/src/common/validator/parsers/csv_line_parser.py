5132d00d8ea21e4f9a482d8bed9c9a8b4073b0bb
Akol125 <akinola.olutola1@nhs.net>
2025-10-21 13:35:57 +0100

    refactoring parser: csv_line_parser

--- lambdas/shared/src/common/validator/parsers/csv_line_parser.py
+++ lambdas/shared/src/common/validator/parsers/csv_line_parser.py
@@ -1,21 +1,34 @@
-# CSV Row importer and data access
+"""
+Parses the CSV, so each header name becomes a key,
+and its corresponding cell value becomes the value.
+
+csv_row : A single line of CSV data ("9011011,Tom,32").
+csv_header : The header line defining column names
+                    ("nhs_number,name,age").
+
+Example:
+    >>> parser = CSVRowParser()
+    >>> parser.parse_row_to_dict("9011011,Tom,32", "nhs_number,name,age")
+    >>> parser.csv_file_data
+    {'nhs_number': '9011011', 'name': 'Tom', 'age': '32'}
+"""
+
 import csv
 
 
 class CSVLineParser:
     # parser variables
     def __init__(self):
-        self.csv_file_data = {}
+        self.csv_file_data: dict[str, str] = {}
 
     # parse the CSV into a Dictionary
-    def parse_csv_line(self, csv_row, csv_header):
+    def parse_csv_line(self, csv_row: str, csv_header: str) -> None:
         # create a key value mapping
         keys = list(csv.reader([csv_header]))[0]
         values = list(csv.reader([csv_row]))[0]
         self.csv_file_data = dict(map(lambda i, j: (i, j), keys, values))
 
-    # retrieve a column of data to work with
-    def get_key_value(self, field_name):
-        # creating empty lists, convert to list
+    # Retrieves the value of a specific column name as a list.
+    def get_key_value(self, field_name: str) -> list[str]:
         data = [self.csv_file_data[field_name]]
         return data
