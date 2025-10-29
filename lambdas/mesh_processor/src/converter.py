03bfa05a5dc6dca73b753f4744abfb3e44895a4d
James Wharmby <james.wharmby1@nhs.net>
2025-10-29 13:02:19 +0000

    ruff

--- lambdas/mesh_processor/src/converter.py
+++ lambdas/mesh_processor/src/converter.py
@@ -17,8 +17,7 @@ s3_client = boto3.client("s3")
 
 def parse_headers(headers_str: str) -> dict[str, str]:
     headers = dict(
-        header_str.split(":", 1) for header_str in headers_str.split("\r\n") 
-        if ":" in header_str
+        header_str.split(":", 1) for header_str in headers_str.split("\r\n") if ":" in header_str
     )  # NOSONAR(S7494) force this to dict
     return {k.strip(): v.strip() for k, v in headers.items()}
 
