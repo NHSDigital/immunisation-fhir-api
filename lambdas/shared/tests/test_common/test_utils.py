c0b5a0215208364fce23469bb170f6c5d7c7ef7f
James Wharmby <james.wharmby1@nhs.net>
2025-11-11 13:58:39 +0000

    move_file II

--- lambdas/shared/tests/test_common/test_utils.py
+++ lambdas/shared/tests/test_common/test_utils.py
@@ -10,7 +10,12 @@ from common.utils import move_file
 @mock_aws
 class TestUtils(unittest.TestCase):
     def setUp(self):
+        self.bucket_name = "move-bucket"
         self.s3_client = boto3.client("s3", region_name="eu-west-2")
+        self.s3_client.create_bucket(
+            Bucket=self.bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
 
         self.logger_info_patcher = patch("logging.Logger.info")
         self.mock_logger_info = self.logger_info_patcher.start()
@@ -20,13 +25,9 @@ class TestUtils(unittest.TestCase):
 
     def test_move_file(self):
         """VED-167 test that the file has been moved to the appropriate location"""
-        bucket_name = "move-bucket"
+        bucket_name = self.bucket_name
         file_key = "src/move_file_test.csv"
         dest_key = "dest/move_file_test.csv"
-        self.s3_client.create_bucket(
-            Bucket=bucket_name,
-            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
-        )
         self.s3_client.put_object(Bucket=bucket_name, Key=file_key, Body="dummy content")
         move_file(bucket_name, file_key, dest_key)
         # Assert the destination object exists
