d7fd7760d87b1b124eac1572b39435c15e07a6bc
James Wharmby <james.wharmby1@nhs.net>
2025-11-21 12:06:12 +0000

    except ValidatorError

--- lambdas/backend/src/service/fhir_service.py
+++ lambdas/backend/src/service/fhir_service.py
@@ -146,7 +146,7 @@ class FhirService:
     def update_immunization(self, imms_id: str, immunization: dict, supplier_system: str, resource_version: int) -> int:
         try:
             self.validator.validate(immunization)
-        except (ValueError, MandatoryError) as error:
+        except (ValidatorError, ValueError, MandatoryError) as error:
             raise CustomValidationError(message=str(error)) from error
 
         existing_immunization, existing_resource_meta = self.immunization_repo.get_immunization_and_resource_meta_by_id(
