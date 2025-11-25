19e94c6cf2249f19e92327fc504207edb0b0909d
Ed Hall <239591530+edhall-nhs@users.noreply.github.com>
2025-11-25 14:24:16 +0000

    Revert "Refactor to not update the resource"
    Change in requirements - we now want to update both identifier_pk and the identifier within the resource
    
    This reverts commit a782fa2c6bfb73b2ab4450eba7469b9b76abdd7c.

--- lambdas/recordforwarder/src/service/fhir_batch_service.py
+++ lambdas/recordforwarder/src/service/fhir_batch_service.py
@@ -4,6 +4,30 @@ from repository.fhir_batch_repository import ImmunizationBatchRepository
 
 IMMUNIZATION_VALIDATOR = ImmunizationValidator()
 
+TPP_V2_SUPPLIER_IDENTIFIER_SYSTEM = "YGA"
+TPP_V5_SUPPLIER_IDENTIFIER_SYSTEM = "https://tpp-uk.com/Id/ve/vacc"
+EMIS_V2_SUPPLIER_IDENTIFIER_SYSTEM = "YGJ"
+EMIS_V5_SUPPLIER_IDENTIFIER_SYSTEM = "https://emishealth.com/identifiers/vacc"
+
+
+def uplift_legacy_identifier(immunization: dict):
+    # This code the above constants can be safely removed once DPS carries out it's data migration to update legacy
+    # identifiers as it should become redundant. However, it may be worth keeping in case legacy format identifiers are
+    # received for some reason. Please see issue VED-904 for more information.
+    identifier = immunization.get("identifier")
+
+    if identifier is None or len(identifier) == 0:
+        # Return here to allow validation to raise appropriate error
+        return
+
+    identifier_system = immunization.get("identifier")[0].get("system")
+
+    if identifier_system == TPP_V2_SUPPLIER_IDENTIFIER_SYSTEM:
+        immunization["identifier"][0]["system"] = TPP_V5_SUPPLIER_IDENTIFIER_SYSTEM
+
+    if identifier_system == EMIS_V2_SUPPLIER_IDENTIFIER_SYSTEM:
+        immunization["identifier"][0]["system"] = EMIS_V5_SUPPLIER_IDENTIFIER_SYSTEM
+
 
 class ImmunizationBatchService:
     def __init__(
@@ -28,6 +52,9 @@ class ImmunizationBatchService:
         the record in the database.
         """
 
+        # TODO: Remove after DPS data migration to new identifiers
+        uplift_legacy_identifier(immunization)
+
         try:
             self.validator.validate(immunization)
         except (ValueError, MandatoryError) as error:
@@ -49,6 +76,9 @@ class ImmunizationBatchService:
         the record in the database.
         """
 
+        # TODO: Remove after DPS data migration to new identifiers
+        uplift_legacy_identifier(immunization)
+
         try:
             self.validator.validate(immunization)
         except (ValueError, MandatoryError) as error:
@@ -69,5 +99,8 @@ class ImmunizationBatchService:
         Exception will be raised if resource didn't exist.Multiple calls to this method won't change
         the record in the database.
         """
+
+        # TODO: Remove after DPS data migration to new identifiers
+        uplift_legacy_identifier(immunization)
 
         return self.immunization_repo.delete_immunization(immunization, supplier_system, vax_type, table, is_present)
