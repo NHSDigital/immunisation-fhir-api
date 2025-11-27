cf1c9b8cc09b9b2b5b579e879b80273a0161a32c
James Wharmby <james.wharmby1@nhs.net>
2025-11-27 16:38:06 +0000

    pathname

--- lambdas/filenameprocessor/src/file_name_processor.py
+++ lambdas/filenameprocessor/src/file_name_processor.py
@@ -272,7 +272,7 @@ def handle_extended_attributes_file(
             FileStatus.PROCESSING,
         )
 
-        copy_file_outside_bucket(bucket_name, file_key, DPS_DESTINATION_BUCKET_NAME, f"archive/{file_key}")
+        copy_file_outside_bucket(bucket_name, file_key, DPS_DESTINATION_BUCKET_NAME, file_key)
         is_file_in_bucket(DPS_DESTINATION_BUCKET_NAME, file_key)
         delete_file(DPS_DESTINATION_BUCKET_NAME, file_key)
 
