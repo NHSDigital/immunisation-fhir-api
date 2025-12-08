49dc3273c850507a67133bc182b967c6e3663776
Matt Jarvis <matt.jarvis2@nhs.net>
2025-12-08 11:40:50 +0000

    VED-728: Store cert etag in case versioning is not enabled on the source bucket.

--- infrastructure/instance/modules/api_gateway/mtls_cert.tf
+++ infrastructure/instance/modules/api_gateway/mtls_cert.tf
@@ -20,8 +20,8 @@ data "aws_s3_object" "cert" {
   key    = local.truststore_file_name
 }
 
-resource "terraform_data" "cert_version" {
-  input = data.aws_s3_object.cert.version_id
+resource "terraform_data" "cert_etag" {
+  input = data.aws_s3_object.cert.etag
 }
 
 resource "aws_s3_bucket" "truststore_bucket" {
@@ -41,6 +41,6 @@ resource "aws_s3_object_copy" "copy_cert_from_storage" {
   key    = local.truststore_file_name
   source = "${data.aws_s3_object.cert.bucket}/${local.truststore_file_name}"
   lifecycle {
-    replace_triggered_by = [terraform_data.cert_version]
+    replace_triggered_by = [terraform_data.cert_etag]
   }
 }
