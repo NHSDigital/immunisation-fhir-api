ec55d70752641c4839d09d0c57c24a1e59c762e4
Akol125 <akinola.olutola1@nhs.net>
2025-12-16 12:06:20 +0000

    use instance level name for dps account

--- infrastructure/instance/file_name_processor.tf
+++ infrastructure/instance/file_name_processor.tf
@@ -268,7 +268,7 @@ resource "aws_iam_policy" "filenameprocessor_dps_extended_attribute_kms_policy" 
           "kms:GenerateDataKey",
           "kms:DescribeKey"
         ],
-        Resource = "arn:aws:kms:eu-west-2:${var.dspp_account_id}:key/*"
+        Resource = "arn:aws:kms:eu-west-2:${var.dspp_core_account_id}:key/*"
       }
     ]
   })
