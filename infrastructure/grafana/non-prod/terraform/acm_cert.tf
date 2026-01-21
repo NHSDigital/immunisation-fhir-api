62223a754ce56614ba1f41debeca9d9579290f96
Akol125 <akinola.olutola1@nhs.net>
2026-01-21 15:11:09 +0000

    use existing alb and add ingress rules for https to sg

--- infrastructure/grafana/non-prod/terraform/acm_cert.tf
+++ infrastructure/grafana/non-prod/terraform/acm_cert.tf
@@ -5,7 +5,7 @@ resource "aws_acm_certificate" "grafana_url_certificate" {
 
 resource "aws_route53_record" "grafana_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.grafana_url.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.grafana_url_certificate.domain_validation_options : dvo.domain_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
       value = dvo.resource_record_value
@@ -20,7 +20,7 @@ resource "aws_route53_record" "grafana_validation" {
 }
 
 resource "aws_acm_certificate_validation" "grafana" {
-  certificate_arn         = aws_acm_certificate.grafana_url.arn
+  certificate_arn         = aws_acm_certificate.grafana_url_certificate.arn
   validation_record_fqdns = [for r in aws_route53_record.grafana_validation : r.fqdn]
 }
 
@@ -33,7 +33,7 @@ resource "aws_lb_listener" "https" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.grafana.arn
+    target_group_arn = aws_alb_target_group.app.arn
   }
 }
 
@@ -50,22 +50,5 @@ resource "aws_lb_listener" "http" {
       protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
-  }
-}
-
-# 3) Target group forwards to Grafana container port (usually 3000)
-resource "aws_lb_target_group" "grafana" {
-  name        = "imms-dev-grafana-tg"
-  port        = var.app_port
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.grafana_main.id
-
-  health_check {
-    path                = "/api/health"
-    matcher             = "200"
-    interval            = 30
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
   }
 }
