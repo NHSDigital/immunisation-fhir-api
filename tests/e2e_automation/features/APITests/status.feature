8f10986845dcf9fd77cefa86b0869077fb57afee
Daniel Yip <daniel.yip4@nhs.net>
2026-01-22 16:31:16 +0000

    VED-1013 Introduce new e2e tests from automation testers (#1146)
    Co-authored-by: Krupasindhu Baral <krupasindhu.baral@nhs.net>
    Co-authored-by: James W. <218585646+JamesW1-NHS@users.noreply.github.com>
    Co-authored-by: fimran08 <90399910+fimran08@users.noreply.github.com>

---
+++ tests/e2e_automation/features/APITests/status.feature
@@ -0,0 +1,23 @@
+@Status_feature @functional
+Feature: Get the API status
+
+@smoke @sandbox
+Scenario: Verify that the /ping endpoint works
+    When I send a request to the ping endpoint
+    Then The request will be successful with the status code '200'
+
+@smoke @sandbox
+Scenario: Verify that the /status endpoint works
+    When I send a request to the status endpoint
+    Then The request will be successful with the status code '200'
+    And The status response will contain a passing healthcheck
+
+@smoke
+Scenario: Verify that clients cannot make a direct connection without mTLS to AWS backend
+    When I send a direct request to the AWS backend
+    Then The request is rejected
+
+@smoke
+Scenario: Verify that unauthenticated clients cannot get a successful response from the API
+    When I send an unauthenticated request to the API
+    Then The request will be unsuccessful with the status code '401'
---
+++ tests/e2e_automation/utilities/apigee/apigee_env_helpers.py
@@ -0,0 +1,51 @@
+import os
+
+PROXY_PR_PREFIX = "immunisation-fhir-api-pr-"
+INT_PROXY_NAME = "immunisation-fhir-api-int"
+
+
+def get_env_var(var_name: str) -> str:
+    value = os.getenv(var_name)
+
+    if not value:
+        raise EnvironmentError(f"{var_name} environment variable is required")
+
+    return value
+
+
+def get_apigee_username() -> str:
+    return get_env_var("APIGEE_USERNAME")
+
+
+def get_proxy_name() -> str:
+    return get_env_var("PROXY_NAME")
+
+
+def use_temp_apigee_apps() -> bool:
+    """
+    Determines if temporary Apigee Apps are required for the test run based on the following business logic:
+    - dynamic PR environments always require temporary apps
+    - Apigee non-prod environments (everything except INT and PROD) use dynamic apps unless the user provides the
+    USE_STATIC_APPS env var to override this
+    """
+    if is_pr_env():
+        return True
+
+    if get_proxy_name() == INT_PROXY_NAME:
+        return False
+
+    return os.getenv("USE_STATIC_APPS", "False") != "True"
+
+
+def is_pr_env() -> bool:
+    """Checks if the tests are running against a dynamic PR environment"""
+    proxy_name = get_proxy_name()
+    return proxy_name.startswith(PROXY_PR_PREFIX)
+
+
+def get_apigee_access_token() -> str:
+    return get_env_var("APIGEE_ACCESS_TOKEN")
+
+
+def get_apigee_environment() -> str:
+    return get_env_var("APIGEE_ENVIRONMENT")
