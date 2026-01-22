8c60b316b79ad029d875d024709812308d06266a
James Wharmby <james.wharmby1@nhs.net>
2026-01-22 12:27:33 +0000

    grafana.tf

---
+++ infrastructure/grafana/non-prod/terraform/grafana.tf
@@ -0,0 +1,41 @@
+# S3 state bucket
+resource "aws_s3_bucket" "grafana_tf_state_bucket" {
+  bucket = "immunisation-grafana-${var.environment}-terraform-state"
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket_versioning" "grafana_tf_state_bucket_versioning" {
+  bucket = aws_s3_bucket.grafana_tf_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Grafana ECR repo
+resource "aws_ecr_repository" "grafana_ecr_repository" {
+  name = "${local.prefix}-app"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "grafana_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.grafana_ecr_repository.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rule_priority = 1
+        description   = "Keep only 10 images"
+        selection = {
+          count_type   = "imageCountMoreThan"
+          count_number = 10
+          tag_status   = "any"
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
