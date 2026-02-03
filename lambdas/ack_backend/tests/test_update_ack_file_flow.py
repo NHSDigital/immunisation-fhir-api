4cba04c51487fe2f869a257dd22611c48c11869a
James Wharmby <james.wharmby1@nhs.net>
2026-02-03 12:59:23 +0000

    archive source file test - to support e2e testing

--- lambdas/ack_backend/tests/test_update_ack_file_flow.py
+++ lambdas/ack_backend/tests/test_update_ack_file_flow.py
@@ -33,6 +33,9 @@ class TestUpdateAckFileFlow(unittest.TestCase):
         self.logger_patcher = patch("update_ack_file.logger")
         self.mock_logger = self.logger_patcher.start()
 
+        self.firehose_patcher = patch("common.clients.global_firehose_client")
+        self.mock_firehose = self.firehose_patcher.start()
+
         self.update_audit_table_item_patcher = patch("update_ack_file.update_audit_table_item")
         self.mock_update_audit_table_item = self.update_audit_table_item_patcher.start()
         self.get_record_and_failure_count_patcher = patch("update_ack_file.get_record_count_and_failures_by_message_id")
@@ -42,6 +45,7 @@ class TestUpdateAckFileFlow(unittest.TestCase):
 
     def tearDown(self):
         self.logger_patcher.stop()
+        self.firehose_patcher.stop()
         self.update_audit_table_item_patcher.stop()
         self.get_record_and_failure_count_patcher.stop()
         self.get_ingestion_start_time_patcher.stop()
@@ -75,3 +79,43 @@ class TestUpdateAckFileFlow(unittest.TestCase):
         # Assert: Only check audit table interactions
         self.mock_get_record_and_failure_count.assert_called_once_with(message_id)
         self.assertEqual(self.mock_update_audit_table_item.call_count, 2)
+
+    def test_source_file_moved_when_ack_process_complete(self):
+        """VED-167 - Test that the source file has been moved correctly"""
+        # Setup
+        message_id = "msg-audit-table"
+        mock_created_at_string = "created_at_formatted_string"
+        file_key = "audit_table_test.csv"
+        self.s3_client.put_object(
+            Bucket=self.source_bucket_name,
+            Key=f"processing/{file_key}",
+            Body="dummy content",
+        )
+        self.s3_client.put_object(
+            Bucket=self.ack_bucket_name, Key=f"TempAck/audit_table_test_BusAck_{mock_created_at_string}.csv"
+        )
+        self.mock_get_record_and_failure_count.return_value = 10, 2
+        self.mock_get_ingestion_start_time.return_value = 1769781283
+
+        # Assert that the source file is not yet in the archive folder
+        with self.assertRaises(self.s3_client.exceptions.NoSuchKey):
+            archived_obj = self.s3_client.get_object(
+                Bucket=self.source_bucket_name,
+                Key=f"archive/{file_key}",
+            )
+
+        # Act
+        update_ack_file.complete_batch_file_process(
+            message_id=message_id,
+            supplier="queue-audit-table-supplier",
+            vaccine_type="vaccine-type",
+            created_at_formatted_string=mock_created_at_string,
+            file_key=file_key,
+        )
+
+        # Assert that the source file has been moved into the archive folder
+        archived_obj = self.s3_client.get_object(
+            Bucket=self.source_bucket_name,
+            Key=f"archive/{file_key}",
+        )
+        self.assertIsNotNone(archived_obj)
