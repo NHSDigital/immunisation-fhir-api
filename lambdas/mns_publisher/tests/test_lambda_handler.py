a20e80bf6691f61642c299e04f95b04d1b2759ce
Daniel Yip <daniel.yip4@nhs.net>
2026-02-11 17:12:28 +0000

    Update lambda function type hinting to reflect new approach

--- lambdas/mns_publisher/tests/test_lambda_handler.py
+++ lambdas/mns_publisher/tests/test_lambda_handler.py
@@ -6,4 +6,4 @@ from lambda_handler import lambda_handler
 
 class TestLambdaHandler(TestCase):
     def test_lambda_handler_returns_true(self):
-        lambda_handler({"Records": [{"eventID": "1234"}]}, Mock())
+        lambda_handler({"Records": [{"messageId": "1234"}]}, Mock())
