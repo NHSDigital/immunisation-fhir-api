5b279f34ec6007f50d79d58776300613ae8adb7a
Ed Hall <239591530+edhall-nhs@users.noreply.github.com>
2026-02-12 15:31:09 +0000

    Set up imms-metrics-dashboard-dev with json file

---
+++ infrastructure/instance/cloudwatch_dashboards.tf
@@ -0,0 +1,5 @@
+resource "aws_cloudwatch_dashboard" "imms-metrics-dashboard-dev" {
+  count          = var.environment == "dev" ? 1 : 0
+  dashboard_name = "imms-metrics-dashboard-dev"
+  dashboard_body = file("${path.root}/imms-metrics-dashboard-dev.json")
+}
