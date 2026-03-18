6e8b2e7d2e25be269acc077013beb0edaef6c7ce
Akshay Shetty <akshay.shetty1@nhs.net>
2026-03-18 21:21:59 +0000

    bug fix to ensure DLQ is activated after retries to PDS

--- infrastructure/instance/sqs_id_sync.tf
+++ infrastructure/instance/sqs_id_sync.tf
@@ -1,7 +1,7 @@
 resource "aws_sqs_queue" "id_sync_queue" {
   name                       = "imms-${local.resource_scope}-id-sync-queue"
   kms_master_key_id          = data.aws_kms_key.existing_id_sync_sqs_encryption_key.arn
-  visibility_timeout_seconds = 360
+  visibility_timeout_seconds = 2160
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.id_sync_dlq.arn
     maxReceiveCount     = 4
