3618152eac5073288b689cccac6b12454103c71d
Akol125 <akinola.olutola1@nhs.net>
2026-03-26 09:16:25 +0000

    fix shared test

--- lambdas/shared/tests/test_common/test_converter_utils.py
+++ lambdas/shared/tests/test_common/test_converter_utils.py
@@ -6,10 +6,10 @@ from common.converter_utils import timestamp_to_rfc3339
 
 class TestTimestampToRfc3339(unittest.TestCase):
     def test_utc_conversion(self):
-        self.assertEqual(timestamp_to_rfc3339("20260212T17443700"), "2026-02-12T17:44:37.000Z")
+        self.assertEqual(timestamp_to_rfc3339("20260212T17443700"), "2026-02-12T17:44:37Z")
 
     def test_bst_conversion(self):
-        self.assertEqual(timestamp_to_rfc3339("20260212T17443701"), "2026-02-12T17:44:37.000+01:00")
+        self.assertEqual(timestamp_to_rfc3339("20260212T17443701"), "2026-02-12T17:44:37+01:00")
 
     def test_too_short_raises(self):
         with self.assertRaises(ValueError):
