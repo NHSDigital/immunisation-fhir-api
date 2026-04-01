025d45c61587aebdc9a1e0b774b566c76fc0a9ec
Thomas-Boyle <45789537+Thomas-Boyle@users.noreply.github.com>
2026-04-01 10:41:08 +0100

    VED-1134: S3 Bucket Policy Missing SecureTransport Statement (#1326)
    * Add HTTPS-only policies for S3 buckets in API Gateway and Splunk modules
    
    - Introduced HTTPS-only IAM policy documents for S3 buckets used in API Gateway and Splunk to enhance security.
    - Updated the S3 bucket policies to enforce HTTPS connections, preventing non-secure access to the stored certificates and logs.
    
    * Refactor S3 bucket policy documents to standardize HTTPS-only enforcement
    
    - Removed unnecessary S3 bucket policy data source.
    - Updated policy statement IDs to "HTTPSOnly" for consistency across IAM policy documents related to S3 buckets in API Gateway.
    
    (cherry picked from commit 9e0b52f8c04c66bfc95738461bb38ff8ba2a2074)

--- infrastructure/instance/modules/splunk/backup.tf
+++ infrastructure/instance/modules/splunk/backup.tf
@@ -3,3 +3,33 @@ resource "aws_s3_bucket" "failed_logs_backup" {
   // To facilitate deletion of non empty busckets
   force_destroy = var.force_destroy
 }
+
+data "aws_iam_policy_document" "failed_logs_backup_https_only" {
+  statement {
+    sid    = "HTTPSOnly"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.failed_logs_backup.arn,
+      "${aws_s3_bucket.failed_logs_backup.arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "failed_logs_backup_https_only" {
+  bucket = aws_s3_bucket.failed_logs_backup.id
+  policy = data.aws_iam_policy_document.failed_logs_backup_https_only.json
+}
