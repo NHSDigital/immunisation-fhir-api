2d4a21e7b4ecadccede42b2773711b7ba32f47e7
Thomas-Boyle <thomasboyle@kainos.com>
2026-04-02 13:36:16 +0100

    Implement ack backend image deployment in CI/CD workflows
    - Added support for building and deploying the ack backend image in the continuous deployment, backend deployment, and PR workflows.
    - Introduced new variables for ack backend image version and build options in the relevant YAML files.
    - Created a new ECR repository for the ack backend and updated the Terraform configuration accordingly.
    - Enhanced the teardown process to clean up the ack backend image references.
    
    This update ensures that the ack backend is properly integrated into the CI/CD pipeline, improving deployment consistency and management.

---
+++ infrastructure/account/ackbackend_ecr_repo.tf
@@ -0,0 +1,59 @@
+resource "aws_ecr_repository" "ackbackend_repository" {
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  image_tag_mutability = "IMMUTABLE"
+  name                 = "imms-ackbackend-repo"
+}
+
+resource "aws_ecr_lifecycle_policy" "ackbackend_repository_lifecycle_policy" {
+  repository = aws_ecr_repository.ackbackend_repository.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep last 10 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 10
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_ecr_repository_policy" "ackbackend_repository_lambda_image_retrieval_policy" {
+  repository = aws_ecr_repository.ackbackend_repository.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrievalPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:DeleteRepositoryPolicy",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:SetRepositoryPolicy"
+        ]
+        Condition = {
+          StringLike = {
+            "aws:sourceArn" = "arn:aws:lambda:${var.aws_region}:${var.imms_account_id}:function:imms-*-ack-lambda"
+          }
+        }
+      }
+    ]
+  })
+}
