81bc02ca2913a9f03057c6efa8692342003a0e33
Thomas-Boyle <45789537+Thomas-Boyle@users.noreply.github.com>
2026-04-20 16:26:35 +0100

    VED-1238: Refactor all remaining lambdas deployment to a dedicated pipeline stage (#1414)
    * Refactor Lambda infrastructure by removing ECR repository and lifecycle policy configurations
    
    - Deleted ECR repository configurations for record processor, batch processor filter, delta, filename processor, id sync, forwarder, redis sync, and mesh processor Lambdas.
    - Removed associated ECR lifecycle policies for the above Lambdas.
    - Updated Lambda functions to use image URIs directly from variables instead of modules.
    - Added new variables for backend, batch processor filter, delta, filename processor, id sync, mesh processor, MNS publisher, record forwarder, and redis sync Lambda image URIs to ensure CI/CD integration.
    - Cleaned up shared Lambda configurations and removed unused local variables.
    
    * Update ECR repository names in CI/CD workflows and Terraform configuration
    
    - Changed ECR repository names for various Lambda functions in the deploy-backend.yml and pr-teardown.yml workflows to reflect updated naming conventions.
    - Updated the lambda_ecr_repos.tf file to align with the new ECR repository names for backend, delta, filename processor, and record forwarder Lambdas, ensuring consistency across the infrastructure configuration.
    
    * Refactor Lambda image handling in workflows to use a unified repository format
    
    * Refactor Lambda deployment workflows to streamline ECR repository configurations
    
    - Updated the deploy-backend.yml workflow to use lambda_dir as the tf_var_suffix for improved clarity and consistency.
    - Simplified the lambda_ecr_repos.tf file by removing redundant local variables and directly defining the lifecycle policy for the recordprocessor repository.
    - Enhanced the ECR repository policy to maintain image retrieval actions in a more structured manner.
    
    * Add lambda ECR repository resource to Terraform configuration
    
    * Refactor ECR repository references in Terraform configuration by removing array indexing for lambda repositories, simplifying the code structure.
    
    ---------
    
    Co-authored-by: Akinola Olutola <akinola.olutola1@nhs.net>

---
+++ infrastructure/account/lambda_ecr_repos.tf
@@ -0,0 +1,143 @@
+locals {
+  lambda_source_arn_prefix = "arn:aws:lambda:${var.aws_region}:${var.imms_account_id}:function:imms-"
+
+  lambda_ecr_repositories = {
+    operation = {
+      name = "imms-backend-repo"
+      lambda_source_names = [
+        "*_get_status",
+        "*_not_found",
+        "*_search_imms",
+        "*_get_imms",
+        "*_delete_imms",
+        "*_create_imms",
+        "*_update_imms"
+      ]
+    }
+    batch_processor_filter = {
+      name                = "imms-batch-processor-filter-repo"
+      lambda_source_names = ["*-batch-processor-filter-lambda"]
+    }
+    delta = {
+      name                = "imms-delta-backend-repo"
+      lambda_source_names = ["*-delta-lambda"]
+    }
+    filenameprocessor = {
+      name                = "imms-filenameprocessor-repo"
+      lambda_source_names = ["*-filenameproc-lambda"]
+    }
+    id_sync = {
+      name                = "imms-id-sync-repo"
+      lambda_source_names = ["*-id-sync-lambda"]
+    }
+    mesh_processor = {
+      name                = "imms-mesh-processor-repo"
+      lambda_source_names = ["*-mesh-processor-lambda"]
+    }
+    mns_publisher = {
+      name                = "imms-mns-publisher-repo"
+      lambda_source_names = ["*-mns-publisher-lambda"]
+    }
+    ack_backend = {
+      name                = "imms-ackbackend-repo"
+      lambda_source_names = ["*-ack-lambda"]
+    }
+    recordforwarder = {
+      name                = "imms-recordforwarder-repo"
+      lambda_source_names = ["*-forwarding-lambda"]
+    }
+    recordprocessor = {
+      name = "imms-recordprocessor-repo"
+      lifecycle_policy = jsonencode({
+        rules = [
+          {
+            rulePriority = 1
+            description  = "Keep last 10 images."
+            selection = {
+              tagStatus   = "any"
+              countType   = "imageCountMoreThan"
+              countNumber = 10
+            }
+            action = {
+              type = "expire"
+            }
+          }
+        ]
+      })
+    }
+    redis_sync = {
+      name                = "imms-redis-sync-repo"
+      lambda_source_names = ["*-redis-sync-lambda"]
+    }
+  }
+}
+#lambda repo
+resource "aws_ecr_repository" "lambda_repository" {
+  for_each = local.lambda_ecr_repositories
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability = "IMMUTABLE"
+  name                 = each.value.name
+}
+
+resource "aws_ecr_repository_policy" "lambda_repository_image_retrieval_policy" {
+  for_each = {
+    for key, repo in local.lambda_ecr_repositories : key => repo if try(repo.lambda_source_names, null) != null
+  }
+
+  repository = aws_ecr_repository.lambda_repository[each.key].name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrievalPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+        Condition = {
+          StringLike = {
+            "aws:sourceArn" = formatlist("${local.lambda_source_arn_prefix}%s", each.value.lambda_source_names)
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_lifecycle_policy" "lambda_repository_lifecycle_policy" {
+  for_each = {
+    for key, repo in local.lambda_ecr_repositories : key => repo if try(repo.lifecycle_policy, null) != null
+  }
+
+  repository = aws_ecr_repository.lambda_repository[each.key].name
+  policy     = each.value.lifecycle_policy
+}
+
+moved {
+  from = aws_ecr_repository.ackbackend_repository
+  to   = aws_ecr_repository.lambda_repository["ack_backend"]
+}
+
+moved {
+  from = aws_ecr_repository_policy.ackbackend_repository_lambda_image_retrieval_policy
+  to   = aws_ecr_repository_policy.lambda_repository_image_retrieval_policy["ack_backend"]
+}
+
+moved {
+  from = aws_ecr_repository.recordprocessor_repository
+  to   = aws_ecr_repository.lambda_repository["recordprocessor"]
+}
+
+moved {
+  from = aws_ecr_lifecycle_policy.recordprocessor_repository_lifecycle_policy
+  to   = aws_ecr_lifecycle_policy.lambda_repository_lifecycle_policy["recordprocessor"]
+}
--- infrastructure/instance/endpoints.tf
+++ infrastructure/instance/endpoints.tf
@@ -12,7 +12,7 @@ module "get_status" {
   prefix                            = local.prefix
   short_prefix                      = local.short_prefix
   function_name                     = "get_status"
-  image_uri                         = module.docker_image.image_uri
+  image_uri                         = var.backend_image_uri
   policy_json                       = data.aws_iam_policy_document.logs_policy_document.json
   error_alarm_notifications_enabled = var.error_alarm_notifications_enabled
   environment                       = var.environment
@@ -65,7 +65,7 @@ module "imms_event_endpoint_lambdas" {
   prefix                            = local.prefix
   short_prefix                      = local.short_prefix
   function_name                     = local.imms_endpoints[count.index]
-  image_uri                         = module.docker_image.image_uri
+  image_uri                         = var.backend_image_uri
   policy_json                       = data.aws_iam_policy_document.imms_policy_document.json
   environment_variables             = local.imms_lambda_env_vars
   vpc_subnet_ids                    = local.private_subnet_ids
