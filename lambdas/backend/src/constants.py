908c1f7d236ed33a2938b1c5316a394d5a6fd328
ariagraham-nhs <aria.graham1@nhs.net>
2026-04-30 10:14:38 +0100

    VED-242: Updated error messaging when search requests exceed 1MB limit (#1448)
    * VED-242: Updated error messaging when search requests exceed 1MB limit
    
    * Rename MAX_SEARCH_RESPONSE_SIZE_BYTES constant

--- lambdas/backend/src/constants.py
+++ lambdas/backend/src/constants.py
@@ -2,4 +2,4 @@
 
 GENERIC_SERVER_ERROR_DIAGNOSTICS_MESSAGE = "Unable to process request. Issue may be transient."
 # Maximum response size for an AWS Lambda function
-MAX_RESPONSE_SIZE_BYTES = 6 * 1024 * 1024
+MAX_SEARCH_RESPONSE_SIZE_BYTES = 1 * 1024 * 1024
